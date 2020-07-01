c981c77c7507b559f3cfa63d7c36277fe676aaa5
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-07-01 17:34:52 +0200

    Removed currently unsupported options.

--- externals/Nandrad/src/NANDRAD_ArgsParser.cpp
+++ externals/Nandrad/src/NANDRAD_ArgsParser.cpp
@@ -20,33 +20,31 @@ Lesser General Public License for more details.
 
 #include "NANDRAD_ArgsParser.h"
 
-#include "NANDRAD_KeywordList.h"
-
 namespace NANDRAD {
 
 ArgsParser::ArgsParser() {
-	addOption(0, "fmu-export", 
+	addOption(0, "fmu-export",
 		"(optional) Generate FMU based on project file specification.",
 		"<FMU file name>",
 		"");
-	addFlag(0, "fmu-modelica-wrapper", 
+	addFlag(0, "fmu-modelica-wrapper",
 		"(optional) If set to 'true' a Modelica wrapper for fmu exported file is created. Only in combination with 'fmu-export'");
-	addOption(0, "fmu-report-format", 
-		"(optional) Special format for FMU export report.",
-		"<FMU report extension>",
-		"");
-	addFlag(0, "designday", 
-		"(optional) Calculate a design day based on project file specification:  true = design day calculation| false = usual calculation.");
-	addFlag(0,  "report",
-		"(optional) Requests a building energy report: true = report is generated| false = report is only generated if requested from project file.");
-	addOption(0, "write-project",
-		"(optional) Write project file including all preprocessor directives and shared output/schedule references.",
-		"<output file name>",
-		"");
-	addOption(0, "output-format",
-		"(optional) Specify data format for model outputs.",
-		"<output file extension>",
-		"d6o");
+//	addOption(0, "fmu-report-format",
+//		"(optional) Special format for FMU export report.",
+//		"<FMU report extension>",
+//		"");
+//	addFlag(0, "designday",
+//		"(optional) Calculate a design day based on project file specification:  true = design day calculation| false = usual calculation.");
+//	addFlag(0,  "report",
+//		"(optional) Requests a building energy report: true = report is generated| false = report is only generated if requested from project file.");
+//	addOption(0, "write-project",
+//		"(optional) Write project file including all preprocessor directives and shared output/schedule references.",
+//		"<output file name>",
+//		"");
+//	addOption(0, "output-format",
+//		"(optional) Specify data format for model outputs.",
+//		"<output file extension>",
+//		"d6o");
 }
 
 } // namespace NANDRAD
