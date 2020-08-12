7d94c513bd5fc65aeb2c62984cfd0144e4f9b1da
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-08-12 13:43:07 +0200

    Added no-gui build argument.

--- build/cmake/run_tests.sh
+++ build/cmake/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # create solver in bb-gcc subdir
-./build.sh 8 release &&
+./build.sh 8 release no-gui &&
 
 # Run test suite in data/tests directory, all project files with .nandrad file extension.
 OMP_NUM_THREADS=1 ../../scripts/TestSuite/run_tests.py -p ../../data/tests -s ./bb-gcc/NandradSolver -e nandrad
