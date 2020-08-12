d9a818474bee339151f124bbe08f26369d6e7e8b
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-08-12 13:35:07 +0200

    Added regression test script.

---
+++ build/cmake/run_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# create solver in bb-gcc subdir
+./build.sh 8 release &&
+
+# Run test suite in data/tests directory, all project files with .nandrad file extension.
+OMP_NUM_THREADS=1 ../../scripts/TestSuite/run_tests.py -p ../../data/tests -s ./bb-gcc/NandradSolver -e nandrad
+
+
