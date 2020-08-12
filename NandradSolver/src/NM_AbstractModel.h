393625be867d529b80c7bb296907c9bbadcbe417
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-08-12 10:34:22 +0200

    Refactored abstract model and made member function implementations optional (for models, that just take inputs, but do not generate results).

--- NandradSolver/src/NM_AbstractModel.h
+++ NandradSolver/src/NM_AbstractModel.h
@@ -47,51 +47,73 @@ public:
 	*/
 	virtual ~AbstractModel();
 
-	/*! Returns a reference type which is unqiue in conjunction with the model ID.
-		The reference type is needed because the id spaces of BIM components and
-		explicit models may overlap. Use ModelIDName, reference type and model ID
-		to unqiuely identify a model (in order to look-up its state object in a vector of state objects).
-		This function must be re-implemented by the user to provide the respective type.
+	/*! Returns a reference type which in conjunction with the model ID allows identification
+		of model object(s), generated from the same parametrization block.
+		This can be a single model object in some cases, but in others (e.g. Zone) there will
+		be several objects with the same reference type (e.g. MRT_ZONE) and the same zone ID.
+		Among these objects, all published variables must have unique names!
+
+		This function must be re-implemented by model objects that provide result variables.
+		Default implementation returns invalid reference type.
 		\sa NANDRAD::ModelInputReference::referenceType_t
 	*/
-	virtual NANDRAD::ModelInputReference::referenceType_t referenceType() const = 0;
+	virtual NANDRAD::ModelInputReference::referenceType_t referenceType() const { return NANDRAD::ModelInputReference::NUM_MRT; }
 
 	/*! Returns a unique class ID name of the implemented model for debugging/output purposes.
-		Must be implemented by derived models.
+		Should be implemented by derived models.
+		Default implementation returns an empty string.
 	*/
-	virtual const char * ModelIDName() const = 0;
+	virtual const char * ModelIDName() const { return ""; }
 
-	/*! Returns unique ID of this abstract state model. */
-	virtual unsigned int id() const = 0;
+	/*! Returns unique ID of this abstract state model.
+		Must be implemented by derived models that generate results.
+		Default implementation returns 0.
+	*/
+	virtual unsigned int id() const { return 0; }
 
-	/*! Returns display name of this abstract state model. */
-	virtual const char * displayName() const= 0;
+	/*! Returns display name of this abstract state model.
+		Should be implemented by derived models.
+		Default implementation returns an empty string.
+	*/
+	virtual const char * displayName() const { return ""; }
 
 	/*! Populates the vector resDesc with descriptions of all results provided by this model.
+		Must be implemented by derived models that generate results.
+		Default implementation does nothing (i.e. no results generated).
+
 		\warning This function generates and populates the vector refDesc from scratch and
-			is not the fastest. If you need to access the reference description several
+			may not be the fastest. If you need to access the reference description several
 			times, consider caching the resulting vector.
 	*/
-	virtual void resultDescriptions(std::vector<QuantityDescription> & resDesc) const = 0;
+	virtual void resultDescriptions(std::vector<QuantityDescription> & resDesc) const { (void)resDesc; }
 
 	/*! Returns vector of all scalar and vector valued results pointer.
 		\param res Vector with pointers to resulting values.
 
 		\todo Clarify, when this is needed...for iterative solution of cycles?
+
+		Default implementation does nothing (i.e. no results generated).
 	*/
-	virtual void resultValueRefs(std::vector<const double *> &res) const = 0;
+	virtual void resultValueRefs(std::vector<const double *> &res) const { (void)res; }
 
 	/*! Looks up a result quantity and returns a pointer to the double value memory location.
+		Must be implemented by derived models that generate results.
+		Default implementation returns a nullptr (i.e. variable does not exist).
+
 		This memory location must be, once reported to the framework, persistant throughout
 		the lifetime of the model object.
+
 		\return A const pointer to the double, or nullptr of no such quantity was found.
 		\note If a vector quantity is requested without indication of an index, the pointer returned points to
 			the begin of the memory array holding the vector data.
 	*/
-	virtual const double * resultValueRef(const QuantityName & quantityName) const = 0;
+	virtual const double * resultValueRef(const QuantityName & quantityName) const { (void)quantityName; return nullptr; }
 
 	/*! Resizes m_results and m_vectorValuedResults vectors.
 		This function is called when all models are already initialized (constructed).
+
+		Must be implemented by derived models that generate results.
+		Default implementation does nothing.
 
 		When re-implementing this class, make sure that m_results and
 		m_vectorValuedResults a fully resized and defined. Ensure, that the functions
@@ -107,8 +129,7 @@ public:
 		Re-implement this function and call DefaultModel::initResults().
 		Resize all vector valued results (m_vectorValuedResults).
 	*/
-	virtual void initResults(const std::vector<AbstractModel*> & /* models */) = 0;
-
+	virtual void initResults(const std::vector<AbstractModel*> & /* models */) {}
 };
 
 
