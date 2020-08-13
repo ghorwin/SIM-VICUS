976ae246f7380e7a90e05d25c9b73c216e0ea4b2
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-08-13 11:16:40 +0200

    RoomStatesModel implemented.

--- NandradSolver/src/NM_RoomStatesModel.cpp
+++ NandradSolver/src/NM_RoomStatesModel.cpp
@@ -19,11 +19,13 @@ Lesser General Public License for more details.
 */
 
 #include <IBK_messages.h>
+#include <IBK_physics.h>
 
 #include "NM_RoomStatesModel.h"
 
 #include <NANDRAD_KeywordList.h>
 #include <NANDRAD_Zone.h>
+#include <NANDRAD_SimulationParameter.h>
 
 //#include "NM_Constants.h"
 //#include "NM_ConstructionStatesModel.h"
@@ -32,19 +34,26 @@ Lesser General Public License for more details.
 //#include "NM_LightingLoadModel.h"
 
 #include "NM_KeywordList.h"
-#include "NM_Schedules.h"
-#include "NM_ThermalComfortModel.h"
 #include "NM_InputReference.h"
 
 
 namespace NANDRAD_MODEL {
 
+RoomStatesModel::RoomStatesModel(unsigned int id, const std::string & displayName)
+	: m_id(id), m_displayName(displayName)
+{
+}
 
-void RoomStatesModel::setup(const NANDRAD::Zone & zone) {
+
+void RoomStatesModel::setup(const NANDRAD::Zone & zone, const NANDRAD::SimulationParameter & simPara) {
 	FUNCID(RoomStatesModel::setup);
 
 	// Only initialization of zone with matching ID allowed
 	IBK_ASSERT(zone.m_id != id());
+
+	// store pointers to the NANDRAD parametrization objects
+	m_zone		= &zone;
+	m_simPara	= &simPara;
 
 	// check for required parameters
 	if (zone.m_para[NANDRAD::Zone::ZP_VOLUME].name.empty())
@@ -52,190 +61,112 @@ void RoomStatesModel::setup(const NANDRAD::Zone & zone) {
 							 .arg(zone.m_id).arg(zone.m_displayName), FUNC_ID);
 
 	// check for valid parameters
-	if (zone.m_para[NANDRAD::Zone::ZP_VOLUME].value <= 0)
+	m_volume = zone.m_para[NANDRAD::Zone::ZP_VOLUME].value;
+	if (m_volume <= 0)
 		throw IBK::Exception(IBK::FormatString("'Volume' in zone #%1 '%2' must be > 0!")
 							 .arg(zone.m_id).arg(zone.m_displayName), FUNC_ID);
+
 	if (!zone.m_para[NANDRAD::Zone::ZP_HEATCAPACITY].name.empty() &&
 		zone.m_para[NANDRAD::Zone::ZP_HEATCAPACITY].value <= 0)
 	{
 		throw IBK::Exception(IBK::FormatString("'HeatCapacity' in zone #%1 '%2' must be > 0!")
 							 .arg(zone.m_id).arg(zone.m_displayName), FUNC_ID);
 	}
+	m_additionalHeatCapacity = zone.m_para[NANDRAD::Zone::ZP_HEATCAPACITY].value;
 
-	// set a pointer to the NANDRAD parametrization objects
-	m_zone					  = &zone;
-}
-
-
-void RoomStatesModel::initResults(const std::vector<AbstractModel*> & models) {
-//	FUNCID(RoomStatesModel::initResults);
-
-	// resize m_results from keyword list
-	DefaultModel::initResults(models);
-
-#if 0
-	for (unsigned int i = 0; i < models.size(); ++i)
-	{
-		const FluidModel* fluid = dynamic_cast<const FluidModel*>(models[i]);
-		// skip models that are not fluids
-		if (fluid == NULL)
-			continue;
-
-		// skip fluids with unsuitable id
-		if (fluid->fluidType() != FluidModel::FT_AIR)
-			continue;
-		// basic fluid model (id = 0) was copied or substituted already
-		if (m_fluid != NULL && fluid->id() == 0)
-			continue;
-
-		// duplicate definition
-		if (m_fluid != NULL && m_fluid->id() != 0 /* && fluid->id() != 0*/) {
-			throw IBK::Exception(IBK::FormatString("Duplicate definition of air model with id #%1 and #%2!")
-				.arg(m_fluid->id()).arg(fluid->id()), FUNC_ID);
-		}
-		// store fluid model
-		m_fluid = fluid;
+	// resize memory cache for results
+	// results depend on calculation mode
+	if (simPara.m_flags[NANDRAD::SimulationParameter::SF_ENABLE_MOISTURE_BALANCE].isEnabled()) {
+		/// \todo hygrothermal implementation
 	}
-	// error: missing fluid model
-	IBK_ASSERT(m_fluid != NULL);
+	else {
+		m_results.resize(1);
 
-	// in the case of explicitely defined fluid model check consistency
-	if (!m_zone->m_intpara[NANDRAD::Zone::ZI_AIRMATERIALREFERENCE].name.empty()) {
-		unsigned int fluidID = m_zone->m_intpara[NANDRAD::Zone::ZI_AIRMATERIALREFERENCE].value;
-		if (fluidID != m_fluid->id()) {
-			throw IBK::Exception(IBK::FormatString("Error initailizing RoomStatesModel with id %1: "
-				"Inconsistant reference to FluidModel with id #%2 in 'FluidReference' tag! "
-				"Expected id #%3!")
-				.arg(id()).arg(fluidID).arg(m_fluid->id()), FUNC_ID);
-		}
+		// cache initial condition
+		m_results[0] = simPara.m_para[NANDRAD::SimulationParameter::SP_INITIAL_TEMPERATURE].value;
 	}
-#endif
 }
 
 
 void RoomStatesModel::resultDescriptions(std::vector<QuantityDescription> & resDesc) const {
-	// fill result definitions from keyword list
-	DefaultModel::resultDescriptions(resDesc);
+	int varCount = 1; // room air temperature is always the first result
+	if (m_simPara->m_flags[NANDRAD::SimulationParameter::SF_ENABLE_MOISTURE_BALANCE].isEnabled()) {
+		varCount = NUM_R; // more variables for hygrothermal calculation
+	}
 
-#if 0
-	// set constraint for room  air temperature
-	std::vector<QuantityDescription>::iterator tempIt = std::find_if(resDesc.begin(), resDesc.end(),
-		FindQuantityDescriptionByName(KeywordList::Keyword("RoomStatesModel::Results", R_AirTemperature)));
-	// room air temperature >= -100°C
-	tempIt->m_minMaxValue = std::make_pair(233.15, std::numeric_limits<double>::max());
+	/// \todo what about CO2 ???
 
-	// add predefined zone parameters
-	for(unsigned int i = 0; i < NANDRAD::Zone::NUM_ZP; ++i)
-	{
-		if(m_zone->m_para[i].name.empty())
-			continue;
-		// skip already prepared quantities
-		if(m_zone->m_para[i].name ==
-			KeywordList::Keyword("RoomStatesModel::InputReferences", InputRef_HeatCapacityAdditionalMass))
-			continue;
-		if (m_zone->m_para[i].name ==
-			KeywordList::Keyword("RoomStatesModel::Results", R_Volume))
-			continue;
-
-		std::vector<QuantityDescription>::iterator paraIt = std::find_if(resDesc.begin(), resDesc.end(),
-			FindQuantityDescriptionByName(m_zone->m_para[i].name));
-		// already defined
-		if (paraIt != resDesc.end())
-			continue;
-
-		// predfined parameters are constants
+	for (int i=0; i<varCount; ++i) {
 		QuantityDescription result;
 		result.m_constant = true;
-		result.m_description = NANDRAD::KeywordList::Description("Zone::para_t",i);
-		result.m_name  = m_zone->m_para[i].name;
-		result.m_unit = m_zone->m_para[i].unit().name();
+		result.m_description = NANDRAD_MODEL::KeywordList::Description("RoomStatesModel::Results", i);
+		result.m_name = NANDRAD_MODEL::KeywordList::Keyword("RoomStatesModel::Results", i);
+		result.m_unit = NANDRAD_MODEL::KeywordList::Unit("RoomStatesModel::Results", i);
+
+		/// \todo constraints?
 		resDesc.push_back(result);
 	}
-	// add generic zone specific parameters from project file as a referencable result
-	std::map<std::string, IBK::Parameter>::const_iterator genericParaIt
-		= m_zone->m_genericParaConst.begin();
-	for( ; genericParaIt != m_zone->m_genericParaConst.end(); ++genericParaIt)
-	{
-		// in gereral parameters are defined as constants
-		QuantityDescription result;
-		result.m_constant = true;
-		result.m_description = std::string();
-		result.m_name  = genericParaIt->second.name;
-		result.m_unit = genericParaIt->second.unit().name();
-		resDesc.push_back(result);
-	}
-	// add space type specific parameters from project file as a referencable result
-	genericParaIt = m_zone->m_spaceTypeRef->m_genericParaConst.begin();
-	for( ; genericParaIt != m_zone->m_spaceTypeRef->m_genericParaConst.end(); ++genericParaIt)
-	{
-		// in gereral parameters are defined as constants
-		QuantityDescription result;
-		result.m_constant = true;
-		result.m_description = std::string();
-		result.m_name  = genericParaIt->second.name;
-		result.m_unit = genericParaIt->second.unit().name();
-		resDesc.push_back(result);
-	}
-#endif
 }
 
-const double * RoomStatesModel::resultValueRef(const QuantityName & quantityName) const {
-#if 0
-	// search inside keyword list result quantities
-	const double *refValue = DefaultModel::resultValueRef(quantityName);
-	if( refValue != NULL)
-		return refValue;
-	// only scalar quantities are allowed any longer
-	if (quantityName.index() != -1)
-		return NULL;
 
-	// check if we request a prededined zone parameter
-	if(NANDRAD::KeywordList::KeywordExists("Zone::para_t",quantityName.name())
-		&& !m_zone->m_para[NANDRAD::KeywordList::Enumeration("Zone::para_t",quantityName.name())].name.empty())
-	{
-		return &m_zone->m_para[NANDRAD::KeywordList::Enumeration("Zone::para_t",quantityName.name())].value;
+const double * RoomStatesModel::resultValueRef(const QuantityName & quantityName) const {
+	// search inside keyword list result quantities
+	// Note: index in m_results corresponds to enumeration values in enum 'Results'
+	const char * const category = "RoomStatesModel::Results";
+
+	if (KeywordList::CategoryExists(category) && KeywordList::KeywordExists(category, quantityName.m_name)) {
+		int resIdx = KeywordList::Enumeration(category, quantityName.m_name);
+		return &m_results[(unsigned int)resIdx];
 	}
-#endif
-	return NULL;
+	else
+		return nullptr;
 }
 
 
 void RoomStatesModel::yInitial(double * y) const {
-#if 0
-	// initial energy density is calculated from initial temperature
-	const double rhoAir		= m_fluid->density(293.15, 0.0);
-	const double cAir		= m_fluid->specificHeatCapacity(293.15, 0.0);
-	const double CMass		= *inputValueRefs()[InputRef_HeatCapacityAdditionalMass];
-	const double Volume	    = m_results[R_Volume].value;
-	const double TInitial	= m_zone->m_para[];
+	// different implementations based on thermal or hygrothermal calculation
+	if (m_simPara->m_flags[NANDRAD::SimulationParameter::SF_ENABLE_MOISTURE_BALANCE].isEnabled()) {
+		/// \todo hygrothermal implementation
+	}
+	else {
 
-	// return initial value to global solver
-	y[0] = TInitial * (rhoAir*cAir*Volume + CMass);
-#endif
+		// initial energy density is calculated from initial temperature
+
+		/// \todo if constants should be adjustable, create parameters in SimulationParameters data section and
+		/// uses these here.
+
+		const double rhoAir		= IBK::RHO_AIR;
+		const double cAir		= IBK::C_AIR;
+		const double TInitial	= m_results[0];
+
+		// return initial value to global solver
+		y[0] = TInitial * (rhoAir*cAir*m_volume + m_additionalHeatCapacity);
+	}
 }
 
-int RoomStatesModel::update() {
-#if 0
+
+int RoomStatesModel::update(const double * y) {
 	// update procedure decomposes all solver quantities
-	IBK_ASSERT(inputValueRefs()[InputRef_InternalEnergy] != NULL);
-	// solver quantities are retrieved by a value reference to the
-	// corresponding RoomBalanceModel
-	double uR = *inputValueRefs()[InputRef_InternalEnergy];
 
-	// check if all references are filled
-	IBK_ASSERT(inputValueRefs()[InputRef_HeatCapacityAdditionalMass] != NULL);
+	// different implementations based on thermal or hygrothermal calculation
+	if (m_simPara->m_flags[NANDRAD::SimulationParameter::SF_ENABLE_MOISTURE_BALANCE].isEnabled()) {
+		/// \todo hygrothermal implementation
+	}
+	else {
+		double uR = y[0];
 
-	// initial energy density is calculated from initial temperature
-	const double rhoAir		= m_fluid->density(293.15, 0.0);
-	const double cAir		= m_fluid->specificHeatCapacity(293.15, 0.0);
-	const double CMass		= *inputValueRefs()[InputRef_HeatCapacityAdditionalMass];
-	const double Volume	    = m_results[R_Volume].value;
+		/// \todo if constants should be adjustable, create parameters in SimulationParameters data section and
+		/// uses these here.
 
-	// calculate room air temperature
-	double TRoom = uR/(rhoAir*cAir*Volume + CMass);
+		const double rhoAir		= IBK::RHO_AIR;
+		const double cAir		= IBK::C_AIR;
 
-	m_results[R_AirTemperature].value = TRoom;
-#endif
+		// calculate room air temperature
+		double TRoom = uR/(rhoAir*cAir*m_volume + m_additionalHeatCapacity);
+
+		m_results[R_AirTemperature] = TRoom;
+	}
+
 	// signal success
 	return 0;
 }
--- NandradSolver/src/NM_RoomStatesModel.h
+++ NandradSolver/src/NM_RoomStatesModel.h
@@ -21,48 +21,58 @@ Lesser General Public License for more details.
 #ifndef RoomStatesModelH
 #define RoomStatesModelH
 
-#include "NM_DefaultModel.h"
+#include "NM_AbstractModel.h"
 
 namespace NANDRAD {
 	class Zone;
+	class SimulationParameter;
 }
 
 namespace NANDRAD_MODEL {
 
-/*!	A model that computes zone states (temperature) out of a given energy density.
+/*!	A model that computes zone states (temperature/humidity) out of a given energy density/RH
 
 	The room state model updates all states from the y-vector and
 	offers it for references from other models. This model is the
-	complement to the RoomBalanceModel which evaluates all heat fluxes
+	complement to the RoomBalanceModel which evaluates all heat/moisture fluxes
 	and calculate divergences(). As the RoomBalanceModel owns the
 	integrator communication facility it also holds the vector of states y
-	(internal energy). Using input references this quantity will be
-	accessed using a constant value reference by the RoomStatesModel.
+	(internal energy etc.).
+
+	Depending on the solver parametrization, this model either has only thermal states (energy density)
+	or hygrothermal states (energy density + air moisture mass density). Depending on
+	the simulation settings, the model offers different result quantities.
+
+	\note This model is a "head"-type model and is evaluated first during NANDRAD model evalution. Hence, all
+		results are "constant" for subsequent models.
+
+	\todo Implement support for hygrothermal states.
 */
-class RoomStatesModel : public DefaultModel {
+class RoomStatesModel : public AbstractModel {
 public:
 
+	/*! Results, published by the model.
+		Availability depends on thermal/hygrothermal calculation modes.
+	*/
 	enum Results {
 		R_AirTemperature,						// Keyword: AirTemperature				[C]		'Room air temperature.'
+		R_RelativeHumidity,						// Keyword: RelativeHumidity			[%]		'Room air relative humidity.'
+		R_VaporPressure,						// Keyword: VaporPressure				[Pa]	'Room air vapor pressure.'
 		NUM_R
 	};
-	enum InputReferences {
-		InputRef_InternalEnergy,				// Keyword: InternalEnergy				[J]		'Internal energy of the room.'
-		NUM_InputRef
-	};
 
-	/*! Constructor, relays ID to DefaultModel constructor. */
-	RoomStatesModel(unsigned int id, const std::string &displayName) :
-		DefaultModel(id, displayName)
-	{
-	}
+	/*! Constructor. */
+	RoomStatesModel(unsigned int id, const std::string &displayName);
+	/*! Copy constructor is not available (disable copy). */
+	RoomStatesModel(const RoomStatesModel &) = delete;
+	/*! Assignment operator is not available (disable copy). */
+	const RoomStatesModel & operator=(const RoomStatesModel &) = delete;
 
 	/*! Initializes model by providing zone parameters.
-		The relevant parameters are cached in the RoomStatesModel results
-		for access by other models.
-		This function also checks for require parameters and tests for validity of values.
+		The relevant parameters are cached in the RoomStatesModel results for access by other models.
+		This function also checks for required parameters and tests for validity of values.
 	*/
-	void setup(const NANDRAD::Zone & zone);
+	void setup(const NANDRAD::Zone & zone, const NANDRAD::SimulationParameter & simPara);
 
 
 	// *** Re-implemented from AbstractModel
@@ -75,6 +85,12 @@ public:
 	/*! Return unique class ID name of implemented model. */
 	virtual const char * ModelIDName() const override { return "RoomStatesModel"; }
 
+	/*! Returns unique ID of this model instance. */
+	virtual unsigned int id() const override { return m_id; }
+
+	/*! Returns display name of this model instance. */
+	virtual const char * displayName() const override { return m_displayName.c_str(); }
+
 	/*! Populates the vector resDesc with descriptions of all results provided by this model. */
 	virtual void resultDescriptions(std::vector<QuantityDescription> & resDesc) const override;
 
@@ -83,8 +99,6 @@ public:
 	*/
 	virtual const double * resultValueRef(const QuantityName & quantityName) const override;
 
-	/*! Resizes m_results vector.*/
-	virtual void initResults(const std::vector<AbstractModel*> & models) override;
 
 
 	// *** Other public member functions
@@ -92,26 +106,45 @@ public:
 	/*! Returns the underlying zone. */
 	const NANDRAD::Zone * zone() const {	return m_zone; }
 
-	/*! Composes all input references.*/
-	void initInputReferences(const std::vector<AbstractModel*> & models);
-
 	/*! Sets initial states in y vector.
 		This function is called after setup(), so that parameters needed for
 		computing the initial condition are already present.
+
+		\param Pointer to the memory array holding all initial states for this room model (to be written into).
+			   The vector has either size 1 for thermal calculations or size 2 for hygrothermal calculations.
 	*/
 	void yInitial(double * y) const;
 
-	/*! Computes room air temperature. */
-	int update();
+	/*! Computes room air temperature.
+		This function is called directly from NandradModel as first step in the model evaluation.
 
-protected:
-
-	/*! Characteristics of fluid (only type air is allowed). Note that fluid must be
-		referenced by an independent FluidModel.
+		\param Pointer to the memory array holding all states for this room model.
+			   The vector has either size 1 for thermal calculations or size 2 for hygrothermal calculations.
 	*/
-//	const FluidModel								*m_fluid;
-	/*! Constant pointer to the referenced zone. */
+	int update(const double * y);
+
+private:
+
+	/*! Constant pointer to the referenced zone parameter block. */
 	const NANDRAD::Zone								*m_zone = nullptr;
+	/*! Reference to simulation parameter block. */
+	const NANDRAD::SimulationParameter				*m_simPara = nullptr;
+
+	/*! Zone ID. */
+	unsigned int									m_id;
+	/*! Display name (for error messages). */
+	std::string										m_displayName;
+
+	/*! Zone air volume in [m3], cached in setup(). */
+	double											m_volume;
+	/*! Additional heat capacity in zone in [J/K], cached in setup(). */
+	double											m_additionalHeatCapacity;
+
+	/*! Data cache for calculated results (updated in call to update()).
+		Index matches enum values of Results.
+	*/
+	std::vector<double>								m_results;
+
 };
 
 } // namespace NANDRAD_MODEL
