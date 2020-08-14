6c7d421aedfef7cbb492d3b6d925bda3daa72a24
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-08-14 07:34:35 +0200

    Test suite script updated.

--- build/cmake/run_tests.sh
+++ build/cmake/run_tests.sh
@@ -3,7 +3,7 @@
 # create solver in bb-gcc subdir
 ./build.sh 8 release no-gui &&
 
-# Run test suite in data/tests directory, all project files with .nandrad file extension.
-OMP_NUM_THREADS=1 ../../scripts/TestSuite/run_tests.py -p ../../data/tests -s ./bb-gcc/NandradSolver/NandradSolver -e nandrad
+# Run test suite in data/tests directory, all project files with .nandrad file extension
+(cd ../../data/tests; ls -R; OMP_NUM_THREADS=1 ../../scripts/TestSuite/run_tests.py -p ./ -s ../../bin/release/NandradSolver -e nandrad)
 
 
