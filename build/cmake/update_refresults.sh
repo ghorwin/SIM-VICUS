e025bacdb775dc79a8469c50afd69f7974a37aa6
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-08-25 14:40:54 +0200

    Added scripts to conveniently update reference results.

---
+++ build/cmake/update_refresults.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+(cd ../../data/tests;../../scripts/TestSuite/update_reference_results.py gcc_linux)
+
+
---
+++ data/tests/update_refresults.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../../scripts/TestSuite/update_reference_results.py gcc_linux
