b8051301297bcfef290bfddf3486213b7cf16a5c
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-08-28 10:25:05 +0200

    Updated TF02 test cases.

---
+++ data/validation/SimQuality/TF02/run.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set solver=..\..\..\..\bin\release_x64\NandradSolver.exe
+
+%solver% SimQuality_TF02.nandrad
+
+pause
---
+++ data/validation/SimQuality/TF02/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+../../../../bin/release/NandradSolver SimQuality_TF02.nandrad
+../../../../bin/release/NandradSolver SimQuality_TF02_Perez.nandrad
+
