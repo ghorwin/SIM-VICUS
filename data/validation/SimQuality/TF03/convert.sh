74aa904437a7dcc63aaff495d89b44fb747286f7
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-09-03 12:24:54 +0200

    Updatd convert script.

--- data/validation/SimQuality/TF03/convert.sh
+++ data/validation/SimQuality/TF03/convert.sh
@@ -4,6 +4,6 @@ python ../../../../scripts/Python/tsvtool.py --output Result.tsv extract_columns
 
 python ../../../../scripts/Python/tsvtool.py --output Result2.tsv extract_columns SimQuality_TF03/results/states.tsv "1|2|3|4" "Variante 1 [C]|Variante 2 [C]|Variante 3 [C]|Variante 4 [C]"
 
-python ../../../../scripts/Python/tsvtool.py --output results/NANDRAD2_TF03_hourly.tsv insert_columns Result.tsv Result2.tsv 2 
+python ../../../../scripts/Python/tsvtool.py --output results/NANDRAD2.tsv insert_columns Result.tsv Result2.tsv 2 
 
 rm Result2.tsv Result.tsv
