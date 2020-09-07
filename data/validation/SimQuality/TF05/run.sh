000c857164c2e30e6e2a55efe97e2b05b65b1e18
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-09-07 10:26:53 +0200

    Added result images.

--- data/validation/SimQuality/TF05/run.sh
+++ data/validation/SimQuality/TF05/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 ../../../../bin/release/NandradSolver SimQuality_TF05.nandrad
-
+#../../../../bin/release/NandradSolver SimQuality_TF05.1.nandrad
+#../../../../bin/release/NandradSolver SimQuality_TF05.2.nandrad
+#../../../../bin/release/NandradSolver SimQuality_TF05.3.nandrad
 
