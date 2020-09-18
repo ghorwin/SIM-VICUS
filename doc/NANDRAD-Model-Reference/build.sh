9f818529b53f56babf9a9ee13ba3792297eb554c
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-09-18 07:37:33 +0200

    Minor documentation review.

--- doc/NANDRAD-Model-Reference/build.sh
+++ doc/NANDRAD-Model-Reference/build.sh
@@ -9,7 +9,7 @@ asciidoctor -a lang=en -r asciidoctor-mathematical $ADOC.adoc &&
 echo &&
 echo '*** Generating pdf ***' &&
 python ../adoc_utils/scripts/adoc-image-prep.py pdf . &&
-asciidoctor-pdf -a lang=en  -a pdf-theme=../adoc_utils/pdf-theme.yml -r asciidoctor-mathematical  -r ../adoc_utils/rouge_theme.rb -a pdf-fontsdir="../adoc_utils/fonts;GEM_FONTS_DIR" $ADOC.adoc &&
+#asciidoctor-pdf -a lang=en  -a pdf-theme=../adoc_utils/pdf-theme.yml -r asciidoctor-mathematical  -r ../adoc_utils/rouge_theme.rb -a pdf-fontsdir="../adoc_utils/fonts;GEM_FONTS_DIR" $ADOC.adoc &&
 
 # restore html-type image files
 echo &&
