76736e812c5af6a636dd8c3565cb77988a752a41
Stephan Hirth <stephan.hirth@tu-dresden.de>
2020-09-18 12:50:26 +0200

    Updated schedules and geometry documentation Added images for print and web

--- doc/NANDRAD-Model-Reference/buildNoMath.sh
+++ doc/NANDRAD-Model-Reference/buildNoMath.sh
@@ -6,10 +6,6 @@ echo '*** Generating html ***' &&
 python ../adoc_utils/scripts/adoc-image-prep.py html . &&
 asciidoctor -a lang=en $ADOC.adoc &&
 
-echo &&
-echo '*** Generating pdf ***' &&
-python ../adoc_utils/scripts/adoc-image-prep.py pdf . &&
-asciidoctor-pdf -a lang=en  -a pdf-theme=../adoc_utils/pdf-theme.yml -r ../adoc_utils/rouge_theme.rb -a pdf-fontsdir="../adoc_utils/fonts;GEM_FONTS_DIR" $ADOC.adoc &&
 
 # restore html-type image files
 echo &&
@@ -23,7 +19,7 @@ if [ ! -d "../../docs/$ADOC" ]; then
 	mkdir ../../docs/$ADOC
 fi &&
 mv $ADOC.html ../../docs/$ADOC/index.html &&
-mv $ADOC.pdf ../../docs &&
+
 
 imgFiles=(./images/*.png) &&
 if [ ${#imgFiles[@]} -gt 0 ]; then
