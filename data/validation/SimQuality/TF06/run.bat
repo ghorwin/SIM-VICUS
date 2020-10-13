1b0f0db3d8ffb177b5f6333d02402fed60b0bf44
Stephan Hirth <stephan.hirth@tu-dresden.de>
2020-10-13 15:02:06 +0200

    Added Test Case 06 from SimQuality

---
+++ data/validation/SimQuality/TF06/run.bat
@@ -0,0 +1,5 @@
+
+cd "..\..\..\..\bin\release_x64\"
+NandradSolver.exe ..\..\data\validation\SimQuality\TF06\SimQuality_TF06.1.nandrad
+
+pause
---
+++ data/validation/SimQuality/TF06/run.sh
@@ -0,0 +1,3 @@
+../../../../bin/release/NandradSolver.exe SimQuality_TF06.1.nandrad
+../../../../bin/release/NandradSolver.exe SimQuality_TF06.2.nandrad
+
