8f23dc1f9bca3a087752fcfccf7444624df1ae95
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-11-04 20:00:27 +0100

    Created validation graphs.

--- data/validation/SimQuality/TF06/run.sh
+++ data/validation/SimQuality/TF06/run.sh
@@ -1,3 +1,5 @@
-../../../../bin/release/NandradSolver.exe SimQuality_TF06.1.nandrad
-../../../../bin/release/NandradSolver.exe SimQuality_TF06.2.nandrad
+#!/bin/bash
+
+../../../../bin/release/NandradSolver SimQuality_TF06.1.nandrad
+../../../../bin/release/NandradSolver SimQuality_TF06.2.nandrad
 
