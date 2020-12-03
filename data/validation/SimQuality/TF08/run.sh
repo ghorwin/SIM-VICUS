d5440ed7550404684b2bd4a6f8606e8e718d8a69
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-12-03 18:07:11 +0100

    Updated validation case TF08

--- data/validation/SimQuality/TF08/run.sh
+++ data/validation/SimQuality/TF08/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-../../../../bin/release/NandradSolver SimQuality_TF07.1.nandrad
-../../../../bin/release/NandradSolver SimQuality_TF07.2.nandrad
-../../../../bin/release/NandradSolver SimQuality_TF07.3.nandrad
-../../../../bin/release/NandradSolver SimQuality_TF07.4.nandrad
+../../../../bin/release/NandradSolver SimQuality_TF08.1.nandrad
+../../../../bin/release/NandradSolver SimQuality_TF08.2.nandrad
+../../../../bin/release/NandradSolver SimQuality_TF08.3.nandrad
+../../../../bin/release/NandradSolver SimQuality_TF08.4.nandrad
 
