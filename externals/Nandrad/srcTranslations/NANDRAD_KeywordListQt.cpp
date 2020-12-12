46a5053211320e37636ea2644bf717a16fe72b46
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2020-12-12 20:38:54 +0100

    Made NANDRAD build the Qt-keyword list (needed for UI).

---
+++ externals/Nandrad/srcTranslations/NANDRAD_KeywordListQt.cpp
@@ -0,0 +1,248 @@
+/*	The NANDRAD data model library.
+
+	Copyright (c) 2012-today, Institut für Bauklimatik, TU Dresden, Germany
+
+	Primary authors:
+	  Andreas Nicolai  <andreas.nicolai -[at]- tu-dresden.de>
+	  Anne Paepcke     <anne.paepcke -[at]- tu-dresden.de>
+
+	This library is part of SIM-VICUS (https://github.com/ghorwin/SIM-VICUS)
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 3 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+*/
+
+#include "NANDRAD_KeywordListQt.h"
+
+namespace NANDRAD {
+KeywordListQt::KeywordListQt() {
+
+	tr("Orientation of the wall [deg].");
+	tr("Inclination of the wall [deg].");
+	tr("Gross area of the wall [m2].");
+	tr("Constant values in defined intervals.");
+	tr("Linear interpolation between values.");
+	tr("Area of the embedded object [m2].");
+	tr("Parametrization of a window model.");
+	tr("Parametrization of a door model.");
+	tr("Parametrization of an opening model.");
+	tr("Dry density of the material.");
+	tr("Specific heat capacity of the material.");
+	tr("Thermal conductivity of the dry material.");
+	tr("Simple pipe at stationary flow conditions without heat exchange");
+	tr("Simple pipe at stationary flow conditions with heat exchange");
+	tr("Pipe with a discretized fluid volume, without heat exchange");
+	tr("Pipe with a discretized fluid volume and heat exchange");
+	tr("A pump with constant pressure.");
+	tr("Simple heat exchanger with given heat flux");
+	tr("A heat pump.");
+	tr("Gas boiler.");
+	tr("Control valve.");
+	tr("Water storage.");
+	tr("Component conditioning system is a system for heating or cooling of components.");
+	tr("Radiator.");
+	tr("Mixer component.");
+	tr("Flow characteristics provided by FMU");
+	tr("Roughness of pipe material.");
+	tr("Pressure loss coefficient for the component.");
+	tr("Maximum pressure loss coefficient for the component.");
+	tr("Inside hydraulic diameter for the component.");
+	tr("External heat transfer coeffient for the outside boundary.");
+	tr("Temperature tolerance for e.g. thermostats.");
+	tr("Pressure head form a pump.");
+	tr("Pump efficiency.");
+	tr("Motor efficiency for a pump.");
+	tr("Rated heating capacity of the component.");
+	tr("Rated Cooling capacity of the component.");
+	tr("Auxiliary power of the component.");
+	tr("Water or air volume of the component.");
+	tr("External surface area of the component.");
+	tr("Convective fraction for heating or cooling.");
+	tr("Coefficient of performance of the component.");
+	tr("heat flux added by heat exchanger");
+	tr("Fixed heat flux");
+	tr("Coupled to external model which requires a heat flux and calculates a temperature in return");
+	tr("Coupled to external model which requires p_in, p_out, T_in and calculates T_out and m_dot");
+	tr("Coupled to external model which requires p_in, m_dot, T_in and calculates T_out and p_out");
+	tr("Length for the element (pipe)");
+	tr("Constant Value");
+	tr("Data file in tsv-format");
+	tr("FMU file");
+	tr("id of coupled zone");
+	tr("Pressure coeffient.");
+	tr("Use results from external wind flow calculation.");
+	tr("Constant heat transfer coefficient.");
+	tr("Constant model.");
+	tr("Constant Long wave emissivity.");
+	tr("Constant model.");
+	tr("Constant Absorption coefficient [0,...,1].");
+	tr("Constant model.");
+	tr("Vapor Transfer Coefficient.");
+	tr("Constant model.");
+	tr("Constant internal loads");
+	tr("Scheduled internal loads");
+	tr("Percentage of equipment load that is radiant emitted.");
+	tr("Percentage of person load that is radiant emitted.");
+	tr("Percentage of lighting load that is radiant emitted.");
+	tr("Complete equipment load per zone floor area.");
+	tr("Complete person load per zone floor area.");
+	tr("Complete lighting load per zone floor area.");
+	tr("Start time point.");
+	tr("End time point.");
+	tr("StepSize.");
+	tr("Some temperatures");
+	tr("Some mass");
+	tr("constant");
+	tr("linear");
+	tr("Continuous data");
+	tr("Annual cycle");
+	tr("Latitude.");
+	tr("Longitude.");
+	tr("Albedo value [0..100 %].");
+	tr("Altitude of building as height above NN [m].");
+	tr("Dry density of the material.");
+	tr("Specific heat capacity of the material.");
+	tr("Thermal conductivity of the dry material.");
+	tr("Model references of climate/location models.");
+	tr("Model references inside a room.");
+	tr("Model references a wall.");
+	tr("Model references an embedded object.");
+	tr("Model references an active object.");
+	tr("Model references scheduled data.");
+	tr("Model references an object list-specific value.");
+	tr("Model references another generic model.");
+	tr("Model references to global physical quantities.");
+	tr("Constant ventilation rate");
+	tr("Scheduled ventilation rate");
+	tr("Ventilation rate");
+	tr("Write values as calculated at output times.");
+	tr("Average values in time (mean value in output step).");
+	tr("Integrate values in time.");
+	tr("All days (Weekend days and Weekdays).");
+	tr("Weekday schedule.");
+	tr("Weekend schedule.");
+	tr("Special Weekday schedule: Monday.");
+	tr("Special Weekday schedule: Tuesday.");
+	tr("Special Weekday schedule: Wednesday.");
+	tr("Special Weekday schedule: Thursday.");
+	tr("Special Weekday schedule: Friday.");
+	tr("Special Weekday schedule: Saturday.");
+	tr("Special Weekday schedule: Sunday.");
+	tr("Holiday schedule.");
+	tr("Monday.");
+	tr("Tuesday.");
+	tr("Wednesday.");
+	tr("Thursday.");
+	tr("Friday.");
+	tr("Saturday.");
+	tr("Sunday.");
+	tr("If enabled, schedules are treated as annually repeating schedules.");
+	tr("X1");
+	tr("X2");
+	tr("I1");
+	tr("I2");
+	tr("ParameterSet1");
+	tr("ParameterSet2");
+	tr("Simple hysteretic shading control based on global radiation sensor");
+	tr("Maximum intensity allowed before shading is closed.");
+	tr("Intensity level below which shading is opened.");
+	tr("Global initial temperature [C].");
+	tr("Global initial relative humidity [%].");
+	tr("Percentage of sensitive heat from domestic water istributed towrads the room.");
+	tr("Air exchange rate resulting from a pressure difference of 50 Pa between inside and outside.");
+	tr("Shielding coefficient for a given location and envelope type.");
+	tr("Ambient temperature for a design day. Parameter that is needed for FMU export.");
+	tr("Start year of the simulation.");
+	tr("Flag activating moisture balance calculation if enabled.");
+	tr("Flag activating CO2 balance calculation if enabled.");
+	tr("Flag activating ventilation through joints and openings.");
+	tr("Flag activating FMU export of climate data.");
+	tr("Distribution based on surface area.");
+	tr("Distribution based on surface type.");
+	tr("Distribution based on zone-specific view factors.");
+	tr("Percentage of solar radiation gains attributed direcly to room [%].");
+	tr("Percentage of surface solar radiation attributed to floor [%].");
+	tr("Percentage of surface solar radiation attributed to roof/ceiling[%].");
+	tr("Percentage of surface solar radiation attributed to walls [%].");
+	tr("Relative tolerance for solver error check.");
+	tr("Absolute tolerance for solver error check.");
+	tr("Maximum permitted time step for integration.");
+	tr("Minimum accepted time step, before solver aborts with error.");
+	tr("Initial time step size (or constant step size for ExplicitEuler integrator).");
+	tr("Coefficient reducing nonlinear equation solver convergence limit.");
+	tr("Coefficient reducing iterative equation solver convergence limit.");
+	tr("Minimum element width for wall discretization.");
+	tr("Stretch factor for variable wall discretizations (0-no disc, 1-equidistance, larger than 1 - variable).");
+	tr("Maximum dimension of a tile for calculation of view factors.");
+	tr("Number of surface discretization elements of a wall in each direction.");
+	tr("Temperature tolerance for ideal heating or cooling.");
+	tr("Relative tolerance for Kinsol solver.");
+	tr("Absolute tolerance for Kinsol solver.");
+	tr("Maximum level of fill-in to be used for ILU preconditioner.");
+	tr("Maximum dimension of Krylov subspace.");
+	tr("Maximum number of nonlinear iterations.");
+	tr("Maximum order allowed for multi-step solver.");
+	tr("Maximum nonlinear iterations for Kinsol solver.");
+	tr("Maximum number of elements per layer.");
+	tr("Check schedules to determine minimum distances between steps and adjust MaxTimeStep.");
+	tr("Disable line search for steady state cycles.");
+	tr("Enable strict Newton for steady state cycles.");
+	tr("CVODE based solver");
+	tr("Explicit Euler solver");
+	tr("Implicit Euler solver");
+	tr("System selects integrator automatically.");
+	tr("Dense solver");
+	tr("KLU sparse solver");
+	tr("GMRES iterative solver");
+	tr("BICGSTAB iterative solver");
+	tr("System selects les solver automatically.");
+	tr("Incomplete LU preconditioner");
+	tr("System selects preconditioner automatically.");
+	tr("Gas layer");
+	tr("Glass layer");
+	tr("Thickness of the window layer.");
+	tr("Thermal conductivity of the window layer.");
+	tr("Mass density of the fill-in gas.");
+	tr("Height of the detailed window.");
+	tr("Width of the detailed window.");
+	tr("Emissivity of surface facing outside.");
+	tr("Emissivity of surface facing inside.");
+	tr("Short wave transmittance at outside directed surface.");
+	tr("Short wave reflectance of surface facing outside.");
+	tr("Short wave reflectance of surface facing inside.");
+	tr("Thermal conductivity of the gas layer.");
+	tr("Dynamic viscosity of the gas layer.");
+	tr("Specific heat capacity of the gas layer.");
+	tr("Standard globbed-layers model.");
+	tr("Detailed window model with layers.");
+	tr("Thermal transmittance");
+	tr("Short wave transmittance at outside directed surface.");
+	tr("Standard reduction factor.");
+	tr("Reduction factor (remaining percentage of solar gains if shading is closed).");
+	tr("Zone with constant/predefined temperatures. (schedule)");
+	tr("Zone described by a temperature node in space.");
+	tr("Ground zone (calculates temperature based on standard).");
+	tr("Temperature of the zone if set constant [C].");
+	tr("Relative humidity of the zone if set constant [%].");
+	tr("CO2 concentration of the zone if set constant [g/m3].");
+	tr("Net usage area of the ground floor [m2] (for area-related outputs and loads).");
+	tr("Zone air volume [m3].");
+	tr("Extra heat capacity [J/K].");
+}
+
+
+QString KeywordListQt::Description( const std::string & category, unsigned int keywordId) { 
+
+	std::string description = KeywordList::Description(category.c_str(), keywordId);
+	return tr(description.c_str());
+
+}
+
+} // namespace
---
+++ externals/Nandrad/srcTranslations/NANDRAD_KeywordListQt.h
@@ -0,0 +1,121 @@
+/*	The NANDRAD data model library.
+
+	Copyright (c) 2012-today, Institut für Bauklimatik, TU Dresden, Germany
+
+	Primary authors:
+	  Andreas Nicolai  <andreas.nicolai -[at]- tu-dresden.de>
+	  Anne Paepcke     <anne.paepcke -[at]- tu-dresden.de>
+
+	This library is part of SIM-VICUS (https://github.com/ghorwin/SIM-VICUS)
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 3 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+*/
+
+#ifndef NANDRAD_KeywordListQtH
+#define NANDRAD_KeywordListQtH
+
+#include <NANDRAD_KeywordList.h>
+#include <QString>
+#include <QColor>
+#include <QCoreApplication>
+
+namespace NANDRAD {
+
+class KeywordListQt {
+Q_DECLARE_TR_FUNCTIONS(KeywordListQt)
+public:
+
+	KeywordListQt();
+
+	/*! Returns a keyword for an enum value t of type enumtype. */
+	static const char * Keyword(const char * const enumtype, int t){ return KeywordList::Keyword( enumtype, t ); }
+
+	/*! Returns a formate keyword string as in "Keyword [Unit]" for an enum value t of type enumtype. */
+	static QString FormatedKeyword(const char * const enumtype, int t){ return QString("%1 [%2]").arg( KeywordList::Keyword( enumtype, t ) ).arg(KeywordList::Unit( enumtype, t ) ); } 
+
+	/*! Returns a translated description for an enum value t of type enumtype.
+		This function throws an exception if the enumeration type is invalid or unknown.
+		If no descrption is given, the keyword itself is returned.
+		\param enumtype 		The full enumeration type including the class name.
+		\param t 				The enumeration type cast in an int.
+		\param no_description	The optional argument is set to true, if there was no description
+		for this keyword, otherwise to false.
+	*/
+	static QString Description(const std::string & category, unsigned int keywordId);
+
+	/*! Returns a default unit for an enum value t of type enumtype.
+		This function throws an exception if the enumeration type is invalid or unknown.
+		Returns an empty string if no default unit was specified.
+		\param enumtype The full enumeration type including the class name.
+		\param t 		The enumeration type cast in an int.
+	*/
+	static const char * Unit(const char * const enumtype, int t){ return KeywordList::Unit( enumtype, t ); }
+
+	/*! Returns a default unit for an enum value t of type enumtype.
+		This function throws an exception if the enumeration type is invalid or unknown.
+		Returns an empty string if no default unit was specified.
+		\param enumtype The full enumeration type including the class name.
+		\param t 		The enumeration type cast in an int.
+	*/
+	static QColor Color(const char * const enumtype, int t){ return QColor( KeywordList::Color( enumtype, t ) ); }
+
+	/*! Returns a default value for an enum value t of type enumtype.
+		This function throws an exception if the enumeration type is invalid or unknown.
+		Returns an nan if no default value was specified.
+		\param enumtype The full enumeration type including the class name.
+		\param t 		The enumeration type cast in an int.
+	*/
+	static double DefaultValue(const char * const enumtype, int t){ return KeywordList::DefaultValue( enumtype, t ); }
+
+	/*! Returns an enumeration value for a given keyword kw of type enumtype.
+		This function throws an exception if the keyword or the enumeration type is invalid or unknown.
+		\param enumtype 	The full enumeration type including the class name.
+		\param kw 			The keyword string.
+		\param deprecated 	The optional argument is set the true if the keyword kw is deprecated.
+	*/
+	static int Enumeration(const char * const enumtype, const std::string & kw, bool * deprecated = nullptr){ return KeywordList::Enumeration( enumtype, kw, deprecated ); }
+
+
+	/*!	Returns the maximum index for entries of a category in the keyword list.
+		This function throws an exception if the enumeration type is invalid or unknown.
+		\param enumtype 	The full enumeration type including the class name.
+	*/
+	static int MaxIndex(const char * const enumtype){ return KeywordList::MaxIndex( enumtype ); }
+
+
+	/*! Returns the number of keywords in this category.
+		This function throws an exception if the enumeration type is invalid or unknown.
+		\param enumtype 	The full enumeration type including the class name.
+	*/
+	static unsigned int Count(const char * const enumtype){ return KeywordList::Count( enumtype ); }
+
+
+	/*! Checks whether a keyword exists in the enumeration of type enumtype.
+		\return Returns true if the keyword is valid, otherwise false.
+	*/
+	static bool KeywordExists(const char * const enumtype, const std::string & kw){ return KeywordList::KeywordExists( enumtype, kw ); }
+
+
+	/*! Checks whether a category of type enumtype exists.
+		\return Returns true if the category/enum type exists, otherwise false.
+	*/
+	static bool CategoryExists(const char * const enumtype){ return KeywordList::CategoryExists( enumtype ); }
+
+
+}; // class
+
+} // namespace 
+/*!
+	\file NANDRAD_KeywordList.h
+	\brief Contains the declaration of class KeywordListQt.
+*/
+
+#endif // NANDRAD_KeywordListQtH
