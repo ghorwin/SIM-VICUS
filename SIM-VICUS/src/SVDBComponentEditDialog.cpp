50ad63d7dd4ad09933a961baadc122661ba6658c
Arombolosh <d-weiss.mail@gmx.de>
2020-12-22 18:33:14 +0100

    add SVDBComponentEditDialog

---
+++ SIM-VICUS/src/SVDBComponentEditDialog.cpp
@@ -0,0 +1,128 @@
+#include "SVDBComponentEditDialog.h"
+#include "ui_SVDBComponentEditDialog.h"
+
+#include <QItemSelectionModel>
+#include <QTableView>
+#include <QSortFilterProxyModel>
+
+#include "SVSettings.h"
+#include "SVStyle.h"
+#include "SVConstants.h"
+#include "SVDBComponentTableModel.h"
+#include "SVDBComponentEditWidget.h"
+
+SVDBComponentEditDialog::SVDBComponentEditDialog(QWidget *parent) :
+	QDialog(parent),
+	m_ui(new Ui::SVDBComponentEditDialog)
+{
+	m_ui->setupUi(this);
+
+	SVStyle::formatDatabaseTableView(m_ui->tableView);
+	m_ui->tableView->horizontalHeader()->setVisible(true);
+
+	m_dbModel = new SVDBComponentTableModel(this, SVSettings::instance().m_db);
+
+	m_proxyModel = new QSortFilterProxyModel(this);
+	m_proxyModel->setSourceModel(m_dbModel);
+	m_ui->tableView->setModel(m_proxyModel);
+
+	m_ui->editWidget->setup(&SVSettings::instance().m_db, m_dbModel);
+
+
+
+}
+
+SVDBComponentEditDialog::~SVDBComponentEditDialog()
+{
+	delete m_ui;
+}
+
+void SVDBComponentEditDialog::on_pushButtonSelect_clicked(){
+	accept();
+}
+
+void SVDBComponentEditDialog::on_pushButtonCancel_clicked() {
+	reject();
+}
+
+
+void SVDBComponentEditDialog::on_pushButtonClose_clicked() {
+	accept();
+}
+
+void SVDBComponentEditDialog::on_toolButtonAdd_clicked() {
+	// add new item
+	QModelIndex sourceIndex = m_dbModel->addNewItem();
+	QModelIndex proxyIndex = m_proxyModel->mapFromSource(sourceIndex);
+	m_ui->tableView->selectionModel()->setCurrentIndex(proxyIndex, QItemSelectionModel::SelectCurrent);
+	// resize ID column
+	sourceIndex = m_dbModel->index(0,SVDBComponentTableModel::ColId);
+	proxyIndex = m_proxyModel->mapFromSource(sourceIndex);
+	if (proxyIndex.isValid())
+		m_ui->tableView->resizeColumnToContents(proxyIndex.column());
+}
+
+
+void SVDBComponentEditDialog::on_toolButtonCopy_clicked() {
+	// determine current item
+	QModelIndex currentProxyIndex = m_ui->tableView->currentIndex();
+	Q_ASSERT(currentProxyIndex.isValid());
+	QModelIndex sourceIndex = m_proxyModel->mapToSource(currentProxyIndex);
+
+	unsigned int id = m_dbModel->data(sourceIndex, Role_Id).toUInt();
+	const VICUS::Component * comp = SVSettings::instance().m_db.m_components[id];
+
+	// add item as copy
+	sourceIndex = m_dbModel->addNewItem(*comp);
+	QModelIndex proxyIndex = m_proxyModel->mapFromSource(sourceIndex);
+	m_ui->tableView->selectionModel()->setCurrentIndex(proxyIndex, QItemSelectionModel::SelectCurrent);
+}
+
+
+void SVDBComponentEditDialog::on_toolButtonRemove_clicked() {
+	QModelIndex currentProxyIndex = m_ui->tableView->currentIndex();
+	Q_ASSERT(currentProxyIndex.isValid());
+	QModelIndex sourceIndex = m_proxyModel->mapToSource(currentProxyIndex);
+	m_dbModel->deleteItem(sourceIndex);
+	// last construction removed? clear input widget
+	if (m_dbModel->rowCount() == 0)
+		onCurrentIndexChanged(QModelIndex(), QModelIndex());
+}
+
+
+void SVDBComponentEditDialog::onCurrentIndexChanged(const QModelIndex &current, const QModelIndex & /*previous*/) {
+	// if there is no selection, deactivate all buttons that need a selection
+	if (!current.isValid()) {
+		m_ui->pushButtonSelect->setEnabled(false);
+		m_ui->toolButtonRemove->setEnabled(false);
+		m_ui->toolButtonCopy->setEnabled(false);
+		m_ui->editWidget->updateInput(-1); // nothing selected
+	}
+	else {
+		m_ui->pushButtonSelect->setEnabled(true);
+		// remove is not allowed for built-ins
+		QModelIndex sourceIndex = m_proxyModel->mapToSource(current);
+		m_ui->toolButtonRemove->setEnabled(!sourceIndex.data(Role_BuiltIn).toBool());
+
+		m_ui->toolButtonCopy->setEnabled(true);
+		m_ui->tableView->selectRow(current.row());
+		// retrieve current component ID
+		int compId = current.data(Role_Id).toInt();
+		m_ui->editWidget->updateInput(compId);
+	}
+}
+
+
+
+void SVDBComponentEditDialog::on_pushButtonReloadUserDB_clicked() {
+	if (QMessageBox::question(this, QString(), tr("Reloading the user database from file will revert all changes made in this dialog since the program was started. Continue?"),
+							  QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes)
+	{
+		// tell db to drop all user-defined materials and re-read the construction DB
+		SVSettings::instance().m_db.m_components.removeUserElements();
+		SVSettings::instance().m_db.readDatabases(SVDatabase::DT_Components);
+		// tell model to reset completely
+		m_dbModel->resetModel();
+	}
+
+}
---
+++ SIM-VICUS/src/SVDBComponentEditDialog.h
@@ -0,0 +1,62 @@
+#ifndef SVDBComponentEditDialogH
+#define SVDBComponentEditDialogH
+
+#include <QDialog>
+
+namespace Ui {
+class SVDBComponentEditDialog;
+}
+
+class QSortFilterProxyModel;
+class QModelIndex;
+
+class SVDBComponentTableModel;
+
+/*! The edit dialog for component types. */
+class SVDBComponentEditDialog : public QDialog
+{
+	Q_OBJECT
+
+public:
+	explicit SVDBComponentEditDialog(QWidget *parent = nullptr);
+	~SVDBComponentEditDialog();
+
+
+	/*! Starts the dialog in "edit components" mode. */
+	void edit();
+
+	/*! Starts the dialog in "select component mode".
+		\return If a component was selected and double-clicked/or the "Select" button was
+				pressed, the function returns the ID of the selected component. Otherwise, if the
+				dialog was aborted, the function returns 0.
+	*/
+	unsigned int select();
+
+private slots:
+	void on_pushButtonSelect_clicked();
+	void on_pushButtonCancel_clicked();
+	void on_pushButtonClose_clicked();
+
+	void on_toolButtonAdd_clicked();
+	void on_toolButtonCopy_clicked();
+	void on_toolButtonRemove_clicked();
+
+	/*! Connected to the respective signal in the table view.
+		Enables/disables the remove button.
+	*/
+	void onCurrentIndexChanged(const QModelIndex &current, const QModelIndex &/*previous*/);
+
+	void on_pushButtonReloadUserDB_clicked();
+
+
+
+private:
+	Ui::SVDBComponentEditDialog *m_ui;
+
+	/*! The component table model (owned). */
+	SVDBComponentTableModel			*m_dbModel		= nullptr;
+	/*! The sort filter model (owned). */
+	QSortFilterProxyModel			*m_proxyModel	= nullptr;
+};
+
+#endif // SVDBComponentEditDialogH
