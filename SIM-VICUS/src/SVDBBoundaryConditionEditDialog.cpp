a93be6e336bb4f7a0adbf03795d6ee7a5935c206
Arombolosh <d-weiss.mail@gmx.de>
2020-12-24 08:34:23 +0100

    update SVDBBoundaryConditionEditDialog

--- SIM-VICUS/src/SVDBBoundaryConditionEditDialog.cpp
+++ SIM-VICUS/src/SVDBBoundaryConditionEditDialog.cpp
@@ -1,14 +1,151 @@
 #include "SVDBBoundaryConditionEditDialog.h"
 #include "ui_SVDBBoundaryConditionEditDialog.h"
 
+#include <QItemSelectionModel>
+#include <QTableView>
+#include <QSortFilterProxyModel>
+
+#include "SVSettings.h"
+#include "SVStyle.h"
+#include "SVConstants.h"
+#include "SVDBModelDelegate.h"
+
+#include "SVDBBoundaryConditionTableModel.h"
+#include "SVDBBoundaryConditionEditWidget.h"
+
 SVDBBoundaryConditionEditDialog::SVDBBoundaryConditionEditDialog(QWidget *parent) :
-	QWidget(parent),
-	ui(new Ui::SVDBBoundaryConditionEditDialog)
+	QDialog(parent),
+	m_ui(new Ui::SVDBBoundaryConditionEditDialog)
 {
-	ui->setupUi(this);
+	m_ui->setupUi(this);
+
+	SVStyle::formatDatabaseTableView(m_ui->tableView);
+	m_ui->tableView->horizontalHeader()->setVisible(true);
+
+	m_dbModel = new SVDBBoundaryConditionTableModel(this, SVSettings::instance().m_db);
+
+	m_proxyModel = new QSortFilterProxyModel(this);
+	m_proxyModel->setSourceModel(m_dbModel);
+	m_ui->tableView->setModel(m_proxyModel);
+
+	m_ui->editWidget->setup(&SVSettings::instance().m_db, m_dbModel);
+
+	connect(m_ui->tableView->selectionModel(), SIGNAL(currentChanged(const QModelIndex &, const QModelIndex &)),
+			this, SLOT(onCurrentIndexChanged(const QModelIndex &, const QModelIndex &)) );
+
+	// set item delegate for coloring built-ins
+	SVDBModelDelegate * dg = new SVDBModelDelegate(this, Role_BuiltIn);
+	m_ui->tableView->setItemDelegate(dg);
+
 }
 
 SVDBBoundaryConditionEditDialog::~SVDBBoundaryConditionEditDialog()
 {
-	delete ui;
+	delete m_ui;
+}
+
+void SVDBBoundaryConditionEditDialog::edit() {
+
+	// hide select/cancel buttons, and show "close" button
+	m_ui->pushButtonClose->setVisible(true);
+	m_ui->pushButtonSelect->setVisible(false);
+	m_ui->pushButtonCancel->setVisible(false);
+
+	m_dbModel->resetModel(); // ensure we use up-to-date data (in case the database data has changed elsewhere)
+
+	// ask database model to update its content
+	m_ui->tableView->resizeColumnsToContents();
+
+	exec();
+}
+
+
+void SVDBBoundaryConditionEditDialog::on_pushButtonSelect_clicked(){
+	accept();
+}
+
+
+void SVDBBoundaryConditionEditDialog::on_pushButtonCancel_clicked() {
+	reject();
+}
+
+
+void SVDBBoundaryConditionEditDialog::on_pushButtonClose_clicked() {
+	accept();
+}
+
+
+void SVDBBoundaryConditionEditDialog::on_toolButtonAdd_clicked() {
+	// add new item
+	QModelIndex sourceIndex = m_dbModel->addNewItem();
+	QModelIndex proxyIndex = m_proxyModel->mapFromSource(sourceIndex);
+	m_ui->tableView->selectionModel()->setCurrentIndex(proxyIndex, QItemSelectionModel::SelectCurrent);
+	// resize ID column
+	sourceIndex = m_dbModel->index(0,SVDBBoundaryConditionTableModel::ColId);
+	proxyIndex = m_proxyModel->mapFromSource(sourceIndex);
+	if (proxyIndex.isValid())
+		m_ui->tableView->resizeColumnToContents(proxyIndex.column());
+}
+
+
+void SVDBBoundaryConditionEditDialog::on_toolButtonCopy_clicked() {
+	// determine current item
+	QModelIndex currentProxyIndex = m_ui->tableView->currentIndex();
+	Q_ASSERT(currentProxyIndex.isValid());
+	QModelIndex sourceIndex = m_proxyModel->mapToSource(currentProxyIndex);
+
+	unsigned int id = m_dbModel->data(sourceIndex, Role_Id).toUInt();
+	const VICUS::BoundaryCondition * comp = SVSettings::instance().m_db.m_boundaryConditions[id];
+
+	// add item as copy
+	sourceIndex = m_dbModel->addNewItem(*comp);
+	QModelIndex proxyIndex = m_proxyModel->mapFromSource(sourceIndex);
+	m_ui->tableView->selectionModel()->setCurrentIndex(proxyIndex, QItemSelectionModel::SelectCurrent);
+}
+
+
+void SVDBBoundaryConditionEditDialog::on_toolButtonRemove_clicked() {
+	QModelIndex currentProxyIndex = m_ui->tableView->currentIndex();
+	Q_ASSERT(currentProxyIndex.isValid());
+	QModelIndex sourceIndex = m_proxyModel->mapToSource(currentProxyIndex);
+	m_dbModel->deleteItem(sourceIndex);
+	// last boundary condition removed? clear input widget
+	if (m_dbModel->rowCount() == 0)
+		onCurrentIndexChanged(QModelIndex(), QModelIndex());
+}
+
+
+void SVDBBoundaryConditionEditDialog::onCurrentIndexChanged(const QModelIndex &current, const QModelIndex & /*previous*/) {
+	// if there is no selection, deactivate all buttons that need a selection
+	if (!current.isValid()) {
+		m_ui->pushButtonSelect->setEnabled(false);
+		m_ui->toolButtonRemove->setEnabled(false);
+		m_ui->toolButtonCopy->setEnabled(false);
+		m_ui->editWidget->updateInput(-1); // nothing selected
+	}
+	else {
+		m_ui->pushButtonSelect->setEnabled(true);
+		// remove is not allowed for built-ins
+		QModelIndex sourceIndex = m_proxyModel->mapToSource(current);
+		m_ui->toolButtonRemove->setEnabled(!sourceIndex.data(Role_BuiltIn).toBool());
+
+		m_ui->toolButtonCopy->setEnabled(true);
+		m_ui->tableView->selectRow(current.row());
+		// retrieve current boundary condition ID
+		int compId = current.data(Role_Id).toInt();
+		m_ui->editWidget->updateInput(compId);
+	}
+}
+
+void SVDBBoundaryConditionEditDialog::on_pushButtonReloadUserDB_clicked() {
+	if (QMessageBox::question(this, QString(), tr("Reloading the user database from file will revert all changes made in this dialog since the program was started. Continue?"),
+							  QMessageBox::Yes | QMessageBox::No) == QMessageBox::Yes)
+	{
+		// tell db to drop all user-defined elements and re-read the DB
+		SVSettings::instance().m_db.m_boundaryConditions.removeUserElements();
+		SVSettings::instance().m_db.readDatabases(SVDatabase::DT_BoundaryConditions);
+		// tell model to reset completely
+		m_dbModel->resetModel();
+	}
+
 }
--- SIM-VICUS/src/SVDBBoundaryConditionEditDialog.h
+++ SIM-VICUS/src/SVDBBoundaryConditionEditDialog.h
@@ -1,22 +1,54 @@
-#ifndef SVDBBOUNDARYCONDITIONEDITDIALOG_H
-#define SVDBBOUNDARYCONDITIONEDITDIALOG_H
+#ifndef SVDBBoundaryConditionEditDialogH
+#define SVDBBoundaryConditionEditDialogH
 
-#include <QWidget>
+#include <QDialog>
 
 namespace Ui {
 class SVDBBoundaryConditionEditDialog;
 }
 
-class SVDBBoundaryConditionEditDialog : public QWidget
-{
+class QSortFilterProxyModel;
+class QModelIndex;
+
+class SVDBBoundaryConditionTableModel;
+
+/*! The edit dialog for component types. */
+class SVDBBoundaryConditionEditDialog : public QDialog{
 	Q_OBJECT
 
 public:
 	explicit SVDBBoundaryConditionEditDialog(QWidget *parent = nullptr);
 	~SVDBBoundaryConditionEditDialog();
 
+	/*! Starts the dialog in "edit boundary condition" mode. */
+	void edit();
+
+private slots:
+	void on_pushButtonSelect_clicked();
+	void on_pushButtonCancel_clicked();
+	void on_pushButtonClose_clicked();
+
+	void on_toolButtonAdd_clicked();
+	void on_toolButtonCopy_clicked();
+	void on_toolButtonRemove_clicked();
+
+	/*! Connected to the respective signal in the table view.
+		Enables/disables the remove button.
+	*/
+	void onCurrentIndexChanged(const QModelIndex &current, const QModelIndex &/*previous*/);
+
+	void on_pushButtonReloadUserDB_clicked();
+
+
+
 private:
-	Ui::SVDBBoundaryConditionEditDialog *ui;
+	Ui::SVDBBoundaryConditionEditDialog		*m_ui;
+
+	/*! The component table model (owned). */
+	SVDBBoundaryConditionTableModel			*m_dbModel		= nullptr;
+	/*! The sort filter model (owned). */
+	QSortFilterProxyModel					*m_proxyModel	= nullptr;
+
 };
 
-#endif // SVDBBOUNDARYCONDITIONEDITDIALOG_H
+#endif // SVDBBoundaryConditionEditDialogH
