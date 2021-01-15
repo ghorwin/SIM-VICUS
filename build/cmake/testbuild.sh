b42f80cd2eea96990d12a203b5cdf35ea97b5cc3
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-01-15 08:22:40 +0100

    Testbuild script updated (now always removes existing bb-gcc dir first)

--- build/cmake/testbuild.sh
+++ build/cmake/testbuild.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# create solver in bb-gcc subdir
+if [ -d "bb-gcc" ]; then
+  rm -rf bb-gcc
+fi &&
+
+# create solver in bb-gcc subdir without OpenMP support
 ./build.sh 8 release no-gui &&
 
 # Run test suite in data/tests directory, all project files with .nandrad file extension
-(cd ../../data/tests; OMP_NUM_THREADS=1 ../../scripts/TestSuite/run_tests.py -p ./ -s ../../bin/release/NandradSolver -e nandrad)
+(cd ../../data/tests; ../../scripts/TestSuite/run_tests.py -p ./ -s ../../bin/release/NandradSolver -e nandrad)
 
 
