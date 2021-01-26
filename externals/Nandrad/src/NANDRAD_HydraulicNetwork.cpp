cf966d2a45cbfa9da891cb03fd733be5f3f92f35
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-01-26 22:38:21 +0100

    Added checks for missing/invalid network parameters.

--- externals/Nandrad/src/NANDRAD_HydraulicNetwork.cpp
+++ externals/Nandrad/src/NANDRAD_HydraulicNetwork.cpp
@@ -6,6 +6,23 @@ namespace NANDRAD {
 void HydraulicNetwork::checkParameters()  {
 	FUNCID(HydraulicNetwork::checkParameters);
 
+	// check our own properties, first
+	switch (m_modelType) {
+		case MT_HydraulicNetwork :
+			m_para[P_DefaultFluidTemperature].checkedValue("DefaultFluidTemperature", "K",
+														   "C", -50, true, 500, true,
+														   "Fluid temperature should be in the range of -50..500 C.");
+		break;
+
+		case NANDRAD::HydraulicNetwork::MT_ThermalHydraulicNetwork:
+			m_para[P_InitialFluidTemperature].checkedValue("InitialFluidTemperature", "K",
+														   "C", -50, true, 500, true,
+														   "Fluid temperature should be in the range of -50..500 C.");
+		break;
+
+		case NANDRAD::HydraulicNetwork::NUM_MT: break; // just to make compiler warnings disappear
+	}
+
 	// check parameters of fluid
 	try {
 		m_fluid.checkParameters(m_modelType);
