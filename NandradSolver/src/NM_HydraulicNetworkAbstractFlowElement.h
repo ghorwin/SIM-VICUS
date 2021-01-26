1682053c614c5fcc3fd9448f70e3c1df35f166ea
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-01-26 22:38:03 +0100

    Documentation added.

--- NandradSolver/src/NM_HydraulicNetworkAbstractFlowElement.h
+++ NandradSolver/src/NM_HydraulicNetworkAbstractFlowElement.h
@@ -1,10 +1,11 @@
 #ifndef HydraulicNetworkAbstractFlowElementH
 #define HydraulicNetworkAbstractFlowElementH
 
-
 namespace NANDRAD_MODEL {
 
-/*! Defines the interface for an abstract flow element. */
+/*! Defines the abstract interface for a flow element.
+	The system function and partial derivative function must be implemented by child objects.
+*/
 class HydraulicNetworkAbstractFlowElement {
 public:
 	HydraulicNetworkAbstractFlowElement() {}
@@ -12,14 +13,17 @@ public:
 	/*! D'tor, definition is in NM_HydraulicNetworkFlowElements.cpp. */
 	virtual ~HydraulicNetworkAbstractFlowElement();
 
+	/*! The flow element's system function. Actually evaluates the residual of the system function
+		for a given combination of mass flux [kg/s], inlet and output pressures [Pa].
+	*/
 	virtual double systemFunction(double mdot, double p_inlet, double p_outlet) const = 0;
 
-	/*! Returns partial derivatives. */
+	/*! Returns partial derivatives of the system functions w.r.t. the three dependent variables. */
 	virtual void partials(double mdot, double p_inlet, double p_outlet,
 						  double & df_dmdot, double & df_dp_inlet, double & df_dp_outlet) const = 0;
 
 	/*! Sets fluid temperature [K] for all internal pipe volumes. */
-	virtual void setFluidTemperature(double fluidTemp) = 0;
+	virtual void setFluidTemperature(double fluidTemp) { (void)fluidTemp; }
 };
 
 } // namespace NANDRAD_MODEL
