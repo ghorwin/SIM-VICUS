f8493dce6440d94d90db62c9a29280634bd2f36d
Hauke Hirsch <hauke.hirsch@tu-dresden.de>
2021-02-15 16:16:11 +0100

    smart select dialog

--- SIM-VICUS/src/SVSmartSelectDialog.cpp
+++ SIM-VICUS/src/SVSmartSelectDialog.cpp
@@ -22,7 +22,16 @@ SVSmartSelectDialog::SVSmartSelectDialog(QWidget *parent) :
 	m_ui->buttonBox->addButton(btn, QDialogButtonBox::AcceptRole);
 	connect(btn, &QPushButton::clicked, this, &SVSmartSelectDialog::onSelectClicked);
 
-	// pupulate combo boxes
+	// populate combo boxes
+
+	m_ui->comboBoxNetwork->clear();
+	m_ui->comboBoxNetwork->addItem(tr("<all networks>"), -1);
+	for (const VICUS::Network & nw : project().m_geometricNetworks)
+		m_ui->comboBoxNetwork->addItem(QString::fromStdString(nw.m_name), nw.m_id);
+	m_ui->comboBoxNetwork->setCurrentIndex(0);
+
+	m_ui->comboBoxNodeType->blockSignals(true);
+	m_ui->comboBoxNodeType->clear();
 	m_ui->comboBoxNodeType->addItem(tr("<all types>"));
 	m_ui->comboBoxNodeType->addItem(VICUS::KeywordList::Keyword("NetworkNode::NodeType", VICUS::NetworkNode::NT_Mixer),
 									VICUS::NetworkNode::NT_Mixer);
@@ -30,8 +39,11 @@ SVSmartSelectDialog::SVSmartSelectDialog(QWidget *parent) :
 									VICUS::NetworkNode::NT_Source);
 	m_ui->comboBoxNodeType->addItem(VICUS::KeywordList::Keyword("NetworkNode::NodeType", VICUS::NetworkNode::NT_Building),
 									VICUS::NetworkNode::NT_Building);
+	m_ui->comboBoxNodeType->blockSignals(false);
 	m_ui->comboBoxNodeType->setCurrentIndex(0);
 
+	m_ui->lineEditMaxHeatingDemandBelow->setEnabled(m_ui->checkBoxMaxHeatingDemandBelow->isChecked());
+	m_ui->lineEditMaxHeatingDemandAbove->setEnabled(m_ui->checkBoxMaxHeatingDemandAbove->isChecked());
 }
 
 
@@ -46,32 +58,48 @@ void SVSmartSelectDialog::onSelectClicked() {
 		// TODO : Building object selections
 	}
 	else {
+
 		// Network selections
+		int filterNetworkId = m_ui->comboBoxNetwork->currentData().toInt();
 
-		// TODO : Hauke, add own selection rules below
-
+		// node selections
 		if (m_ui->tabWidgetNetworkSelections->currentIndex()==0) {
-			// node selections
 
 			VICUS::NetworkNode::NodeType nodeTypeFilter = VICUS::NetworkNode::NUM_NT; // no filter on node type
 			if (m_ui->comboBoxNodeType->currentIndex() != 0)
 				nodeTypeFilter = (VICUS::NetworkNode::NodeType)m_ui->comboBoxNodeType->currentData().toInt();
 
-			// TODO : more filters
+			// heating demand must be below value
+			double maxHeatingDemandBelowFilter = std::numeric_limits<double>::max();
+			if (m_ui->checkBoxMaxHeatingDemandBelow->isChecked() && m_ui->lineEditMaxHeatingDemandBelow->isValid())
+				maxHeatingDemandBelowFilter = m_ui->lineEditMaxHeatingDemandBelow->value();
 
+			// heating demand must be above value
+			double maxHeatingDemandAboveFilter = std::numeric_limits<double>::lowest();
+			if (m_ui->checkBoxMaxHeatingDemandAbove->isChecked() && m_ui->lineEditMaxHeatingDemandAbove->isValid())
+				maxHeatingDemandAboveFilter = m_ui->lineEditMaxHeatingDemandAbove->value();
 
 			// now process all network objects and store IDs of selected nodes
 			std::set<unsigned int> nodeIDs;
 
 			for (const VICUS::Network & nw : project().m_geometricNetworks) {
+
+				// filter networks
+				if (filterNetworkId != -1 && filterNetworkId != (int)nw.m_id)
+					continue;
+
 				for (const VICUS::NetworkNode & n : nw.m_nodes) {
-					// apply filter rules
+				// apply filter rules
 
 					// filter based on type
 					if (nodeTypeFilter != VICUS::NetworkNode::NUM_NT && n.m_type != nodeTypeFilter)
 						continue;
 
-					// TODO : more filters
+					if (n.m_maxHeatingDemand > maxHeatingDemandBelowFilter)
+						continue;
+
+					if (n.m_maxHeatingDemand < maxHeatingDemandAboveFilter)
+						continue;
 
 					// all filters match, store unique ID of this node
 					nodeIDs.insert(n.uniqueID());
@@ -82,9 +110,76 @@ void SVSmartSelectDialog::onSelectClicked() {
 			undo->push();
 		}
 		else {
-			// edge selections
+
+			// length must be below value
+			double lengthBelowFilter = std::numeric_limits<double>::max();
+			if (m_ui->checkBoxLengthBelow->isChecked() && m_ui->lineEditLengthBelow->isValid())
+				lengthBelowFilter = m_ui->lineEditLengthBelow->value();
+
+			// length must be above value
+			double lengthAboveFilter = std::numeric_limits<double>::lowest();
+			if (m_ui->checkBoxLengthAbove->isChecked() && m_ui->lineEditLengthAbove->isValid())
+				lengthAboveFilter = m_ui->lineEditLengthAbove->value();
+
+			std::set<unsigned int> edgeIDs;
+
+			for (const VICUS::Network & nw : project().m_geometricNetworks) {
+
+				// filter networks
+				if (filterNetworkId != -1 && filterNetworkId != (int)nw.m_id)
+					continue;
+
+				for (const VICUS::NetworkEdge & edge : nw.m_edges) {
+
+					if (edge.length() > lengthBelowFilter)
+						continue;
+
+					if (edge.length() < lengthAboveFilter)
+						continue;
+
+					// all filters match, store unique ID of this node
+					edgeIDs.insert(edge.uniqueID());
+				}
+			}
+
+			SVUndoTreeNodeState * undo = new SVUndoTreeNodeState(tr("Selecting edges"), SVUndoTreeNodeState::SelectedState, edgeIDs, true);
+			undo->push();
 		}
 	}
 
 	// when this even is done, the dialog is closed automatically
+}
+
+
+void SVSmartSelectDialog::on_comboBoxNodeType_currentIndexChanged(int index)
+{
+	bool noBuilding = index==1 || index==2;
+	m_ui->checkBoxMaxHeatingDemandAbove->setEnabled(!noBuilding);
+	m_ui->lineEditMaxHeatingDemandAbove->setEnabled(!noBuilding);
+	m_ui->checkBoxMaxHeatingDemandBelow->setEnabled(!noBuilding);
+	m_ui->lineEditMaxHeatingDemandBelow->setEnabled(!noBuilding);
+	if (noBuilding){
+		m_ui->checkBoxMaxHeatingDemandBelow->setChecked(false);
+		m_ui->checkBoxMaxHeatingDemandAbove->setChecked(false);
+	}
+}
+
+void SVSmartSelectDialog::on_checkBoxMaxHeatingDemandBelow_clicked(bool checked)
+{
+	m_ui->lineEditMaxHeatingDemandBelow->setEnabled(checked);
+}
+
+void SVSmartSelectDialog::on_checkBoxMaxHeatingDemandAbove_clicked(bool checked)
+{
+	m_ui->lineEditMaxHeatingDemandAbove->setEnabled(checked);
+}
+
+void SVSmartSelectDialog::on_checkBoxLengthBelow_clicked(bool checked)
+{
+	m_ui->lineEditLengthBelow->setEnabled(checked);
+}
+
+void SVSmartSelectDialog::on_checkBoxLengthAbove_clicked(bool checked)
+{
+	m_ui->lineEditLengthAbove->setEnabled(checked);
 }
--- SIM-VICUS/src/SVSmartSelectDialog.h
+++ SIM-VICUS/src/SVSmartSelectDialog.h
@@ -18,6 +18,16 @@ public:
 private slots:
 	void onSelectClicked();
 
+	void on_comboBoxNodeType_currentIndexChanged(int index);
+
+	void on_checkBoxMaxHeatingDemandBelow_clicked(bool checked);
+
+	void on_checkBoxMaxHeatingDemandAbove_clicked(bool checked);
+
+	void on_checkBoxLengthBelow_clicked(bool checked);
+
+	void on_checkBoxLengthAbove_clicked(bool checked);
+
 private:
 	Ui::SVSmartSelectDialog *m_ui;
 };
