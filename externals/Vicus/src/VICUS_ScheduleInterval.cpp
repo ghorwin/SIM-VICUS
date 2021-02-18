6240bfa649a40d12c94628db1c86e284548339f6
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-02-18 16:34:57 +0100

    isValid() checked

--- externals/Vicus/src/VICUS_ScheduleInterval.cpp
+++ externals/Vicus/src/VICUS_ScheduleInterval.cpp
@@ -8,7 +8,7 @@ namespace VICUS {
 
 bool ScheduleInterval::isValid() const {
 	// check that interval start day is in range 0..364
-	if(m_intervalStartDay < 0 || m_intervalStartDay>364)
+	if (m_intervalStartDay>364)
 		return false;
 
 	// check that the daily cycles define indeed all days
@@ -16,7 +16,32 @@ bool ScheduleInterval::isValid() const {
 	if(m_dailyCycles.empty())
 		return false;
 
-	std::set<int>	allDayTypes, checkDayTypes;
+	// we need to check two things:
+	// - all day times must be parametrized
+	// - there must not be duplicate day type definitions
+
+	std::set<int>	parametrizedDayTypes;
+	// process all daily cycles
+	for (const DailyCycle & dC : m_dailyCycles) {
+		// check if DailyCycle itself is correct (contains correct day types)
+		if (!dC.isValid())
+			return false;
+
+		// check if any of the parametrized day types has previously been defined
+		// and remember day typers
+
+		for ( int dT : dC.m_dayTypes) {
+			if (parametrizedDayTypes.find(dT) != parametrizedDayTypes.end())
+				return false; // already in set, duplicate!
+
+			parametrizedDayTypes.insert(dT);
+		}
+	}
+	// Holidays are optional, so we just insert this day type
+	parametrizedDayTypes.insert(NANDRAD::Schedule::ST_HOLIDAY);
+
+	// now remove all days that we require
+	std::set<int>	allDayTypes;
 	allDayTypes.insert(NANDRAD::Schedule::ST_MONDAY);
 	allDayTypes.insert(NANDRAD::Schedule::ST_TUESDAY);
 	allDayTypes.insert(NANDRAD::Schedule::ST_WEDNESDAY);
@@ -26,15 +51,8 @@ bool ScheduleInterval::isValid() const {
 	allDayTypes.insert(NANDRAD::Schedule::ST_SUNDAY);
 	allDayTypes.insert(NANDRAD::Schedule::ST_HOLIDAY);
 
-	for(auto &  dC : m_dailyCycles){
-		if(!dC.isValid())
-			return false;
-
-		for( int dT : dC.m_dayTypes)
-			checkDayTypes.insert(dT);
-	}
-
-	if(allDayTypes != checkDayTypes)
+	// both sets must contain all required day types
+	if (allDayTypes != parametrizedDayTypes)
 		return false;
 
 	return true;
