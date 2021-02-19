af326cf4acee8466d337d913d864b29de40b7829
Arombolosh <d-weiss.mail@gmx.de>
2021-02-19 10:57:19 +0100

    add period date dialog

---
+++ SIM-VICUS/src/SVDBScheduleCreatePeriod.cpp
@@ -0,0 +1,25 @@
+#include "SVDBScheduleCreatePeriod.h"
+#include "ui_SVDBScheduleCreatePeriod.h"
+
+SVDBScheduleCreatePeriod::SVDBScheduleCreatePeriod(QWidget *parent) :
+	QWidget(parent),
+	m_ui(new Ui::SVDBScheduleCreatePeriod)
+{
+	m_ui->setupUi(this);
+	hide();
+	setWindowFlag(Qt::WindowStaysOnTopHint);
+}
+
+SVDBScheduleCreatePeriod::~SVDBScheduleCreatePeriod()
+{
+	delete m_ui;
+}
+
+void SVDBScheduleCreatePeriod::on_toolButtonApply_clicked(){
+	emit dayChanged(m_ui->dateEdit->date().dayOfYear()-1);
+	close();
+}
+
+void SVDBScheduleCreatePeriod::on_toolButtonRemove_clicked(){
+	close();
+}
