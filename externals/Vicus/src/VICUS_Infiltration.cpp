7fa1ed27875765dd238e47259a45a60d1834c009
Arombolosh <d-weiss.mail@gmx.de>
2021-04-07 14:26:58 +0200

    edit infiltration isValid()

--- externals/Vicus/src/VICUS_Infiltration.cpp
+++ externals/Vicus/src/VICUS_Infiltration.cpp
@@ -1,11 +1,44 @@
 #include "VICUS_Infiltration.h"
 
+#include "VICUS_KeywordList.h"
+
 namespace VICUS {
+
 
 
 bool Infiltration::isValid() const
 {
 	///TODO DIRK/KATJA
+	if(m_id == INVALID_ID)
+		return false;
+
+	if(m_airChangeType == NUM_AC)
+		return false;
+
+	if(m_managementScheduleId == INVALID_ID)
+		return false;
+	else{
+		/// TODO Dirk->Andreas wie komm ich jetzt an die Schedule Datenbank und kann die vorgegebene ID prüfen
+		/// ob dieser Schedule valide ist?
+	}
+	try {
+		m_para->checkedValue(VICUS::KeywordList::Keyword("Infiltration::para_t", P_AirChangeRate),
+							 "1/h", "1/h", 0, true, 100, true, nullptr);
+
+	}  catch (...) {
+		return false;
+	}
+
+	if(m_airChangeType == AC_n50){
+		try {
+			m_para[P_ShieldingCoefficient].checkedValue(VICUS::KeywordList::Keyword("Infiltration::para_t", P_ShieldingCoefficient),
+														"-", "-", 0, true, 10, true, nullptr);
+
+		}  catch (...) {
+			return false;
+		}
+	}
+
 	return true;
 }
 
