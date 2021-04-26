251276028a4f5cab84c3e6b556b4a7d822c58015
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-04-26 08:29:30 +0200

    NANDRAD data structure for ideal heating-cooling finished. Test case added.

--- externals/Nandrad/src/NANDRAD_IdealHeatingCoolingModel.cpp
+++ externals/Nandrad/src/NANDRAD_IdealHeatingCoolingModel.cpp
@@ -5,7 +5,14 @@
 namespace NANDRAD {
 
 void IdealHeatingCoolingModel::checkParameters() const {
-	// TODO Dirk
+	m_para[P_MaxHeatingPowerPerArea].checkedValue("MaxHeatingPowerPerArea", "W/m2K", "W/m2K",
+											   0, true,
+											   std::numeric_limits<double>::max(), true,
+											   "Maximum for heating power must be >= 0 W/m2K.");
+	m_para[P_MaxCoolingPowerPerArea].checkedValue("MaxCoolingPowerPerArea", "W/m2K", "W/m2K",
+											   0, true,
+											   std::numeric_limits<double>::max(), true,
+											   "Maximum for cooling power must be >= 0 W/m2K.");
 }
 
 
