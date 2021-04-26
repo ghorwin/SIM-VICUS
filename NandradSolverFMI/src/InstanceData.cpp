7ae46e8a88ec288160f0bf64ba34dadc1afaa6e6
KnutKnudson <Anne.Paepcke@tu-dresden.de>
2021-04-26 16:59:39 +0200

    Added support of simple forward Co-simulation (without buffering or rest).

--- NandradSolverFMI/src/InstanceData.cpp
+++ NandradSolverFMI/src/InstanceData.cpp
@@ -77,12 +77,87 @@ void InstanceData::init() {
 
 	logger(fmi2OK, "progress", "Starting initialization.");
 
-	// TODO
-
 	// Model Init
 	try {
 
 		// TODO
+		// Generate project filename
+		IBK::Path projectFile = IBK::Path::fromURI(m_resourceLocation);
+		projectFile /= "Project.nandrad"; // append project file, always the same within FMU container
+
+		// Create and customize NANDRAD::ArgsParser
+		NANDRAD::ArgsParser args;
+		args.m_projectFile = projectFile.str();
+
+		IBK::Path outputDirectory = IBK::Path(m_resultsRootDir);
+		// error: master didn't specify the ResultsRootDir parameter or no directory is defined
+		if (m_resultsRootDir.empty()) {
+			 //m_resultsRootDir now holds the default/fall-back output root dir
+			 throw IBK::Exception( IBK::FormatString("No output directory is specified inside string parameter "
+				"'resultRootDir'."), FUNC_ID);
+		}
+
+		// clear path from 'IBK/Nandrad'
+		IBK::Path resultsParentDir = outputDirectory;
+		if(resultsParentDir.filename() == "Nandrad") {
+			try {
+				if(resultsParentDir.parentPath().filename() == "IBK") {
+					resultsParentDir = resultsParentDir.parentPath().parentPath();
+				}
+			}
+			catch(IBK::Exception &ex) {
+				throw IBK::Exception(ex, IBK::FormatString("Invalid results root directory '%1'.").arg(outputDirectory.str()),
+									  FUNC_ID);
+			}
+		}
+
+		// check if base directory exists
+		if(!resultsParentDir.exists()) {
+			throw IBK::Exception( IBK::FormatString("Invalid results parent directory '%1'.").arg(outputDirectory.str()),
+								  FUNC_ID);
+		}
+
+		// append "results" for the actual output files
+		outputDirectory /= "results";
+		if (!IBK::Path::makePath(outputDirectory)) {
+			throw IBK::Exception( IBK::FormatString("Cannot create output directory '%1'.").arg(outputDirectory.str()),
+								  FUNC_ID);
+		}
+
+		args.setOption(args.keyword(IBK::SolverArgsParser::GO_OUTPUT_DIR), m_resultsRootDir);
+
+		// *** create directory structure ***
+		try {
+			IBK::Path executablePath = args.m_projectFile.parentPath().parentPath() / "binaries";
+			m_model.setupDirectories(args);
+			// *** create message handler and log file ***
+			setupMessageHandler(m_model.dirs().m_logDir / "screenlog.txt");
+			// now we have a log directory and can write our messages to the log file
+		}
+		catch (IBK::Exception &ex) {
+			throw IBK::Exception(ex, "Error creating project/ output directories.", FUNC_ID);
+		}
+
+		// *** write program/copyright info ***
+		IBK::IBK_Message(PROGRAM_INFO, IBK::MSG_PROGRESS, FUNC_ID, IBK::VL_STANDARD);
+		NANDRAD_MODEL::NandradModel::printVersionStrings();
+		SOLFRA::SolverControlFramework::printVersionInfo();
+		IBK::IBK_Message("\n", IBK::MSG_PROGRESS, FUNC_ID, IBK::VL_STANDARD);
+
+		// *** initialize model ***
+		try {
+			m_model.init(args);
+
+			// set start time
+			if (m_tStart != 0.0) {
+				IBK::IBK_Message( IBK::FormatString("Settings simulation start time to t=%1 s (= %2)\n").arg(m_tStart).arg(IBK::Time::format_time_difference(m_tStart)),
+								  IBK::MSG_PROGRESS, FUNC_ID, IBK::VL_STANDARD);
+//				const_cast<NANDRAD::Project&>(m_model).m_simulationParameter.m_interval.m_para[NANDRAD::Interval::IP_START].value = m_tStart;
+			}
+		}
+		catch (IBK::Exception &ex) {
+			throw IBK::Exception(ex, "Model initialization error.", FUNC_ID);
+		}
 
 		logger(fmi2OK, "progress", "Initialization complete.");
 		IBK::IBK_Message("Model initialization finished.\n", IBK::MSG_PROGRESS, FUNC_ID, IBK::VL_STANDARD);
@@ -108,9 +183,90 @@ void InstanceData::integrateTo(double tCommunicationIntervalEnd) {
 //	NANDRAD_MODEL::NandradModel::m_self = dynamic_cast<NANDRAD_MODEL::NandradModel*>(modelInterface);
 
 	try {
-//		double tCommunicationIntervalStart = modelInterface->integratorInterface()->t();
+		double tCommunicationIntervalStart = m_model.integratorInterface()->t();
+		// reset is not implemented, yet
+		if(tCommunicationIntervalStart > tCommunicationIntervalEnd) {
+			throw IBK::Exception(IBK::FormatString("Error performing integration step: "
+				"reset from integrator time point %1 to the new communication interval end %2 is not "
+				" supported, yet!")
+				.arg(tCommunicationIntervalStart)
+				.arg(tCommunicationIntervalEnd),
+				FUNC_ID);
+		}
 
-		// TODO
+		/* 1.) Set a new tStop value (= tCommunicationIntervalEnd) in integrator*/
+		SOLFRA::IntegratorInterface *integrator		 = m_model.integratorInterface();
+
+		SOLFRA::IntegratorErrorControlled* integratorErrorControlled =
+			dynamic_cast<SOLFRA::IntegratorErrorControlled*> (integrator);
+		// stop value is still only defined for error controled integrators
+		if(  integratorErrorControlled != nullptr)
+			integratorErrorControlled->m_stopTime = tCommunicationIntervalEnd;
+
+		/* 3.) integration loop until communication interval has been reached:*/
+		SOLFRA::OutputScheduler		*outputScheduler = m_model.outputScheduler();
+
+
+		// outputs are only written if we start the simulation from begin, therefore
+		// we pass t0 and y0 for the initial model evaluation within writeOutputs()
+		if (tCommunicationIntervalStart == m_model.t0()) {
+			m_model.setTime(m_model.t0());
+			m_model.setY(m_model.y0());
+			m_model.ydot(nullptr);
+//			m_model.pushOutputOnBuffer( m_model.t0(), m_model.y0());
+		}
+//		else {
+//			// reset outputs if necessary
+//			m_model.resetOutputBuffer(tCommunicationIntervalStart);
+//		}
+
+		m_model.startCommunicationInterval(tCommunicationIntervalStart);
+		// integration loop
+		double t = tCommunicationIntervalStart;
+		double tOutput = outputScheduler->nextOutputTime(t);
+
+		while (t < tCommunicationIntervalEnd) {
+
+			// (contains parallel code)
+			SOLFRA::IntegratorInterface::StepResultType res =integrator->step();
+			if (res != SOLFRA::IntegratorInterface::StepSuccess) {
+				throw IBK::Exception("Integrator step failed!", FUNC_ID);
+			}
+
+			// get new time point and time step size
+			t = integrator->t();
+
+			// notify model of completed step (contains parallel code)
+			const double * y_current = integrator->yOut( t );
+
+			m_model.stepCompleted(t, y_current);
+
+			while (tOutput <= t) {
+
+				// retrieve interpolated states at output time point (contains parallel code)
+				const double * yOutput = integrator->yOut(tOutput);
+				double tOutputNext = 0.0;
+
+				// tell m_model to write outputs
+				m_model.setTime(tOutput);
+				m_model.setY(yOutput);
+				m_model.ydot(nullptr);
+//				m_model.pushOutputOnBuffer( tOutput, yOutput);
+
+				// retrieve new output time point
+				tOutputNext = outputScheduler->nextOutputTime(tOutput);
+				if (tOutputNext <= tOutput)
+					throw IBK::Exception(IBK::FormatString("Output scheduler returned output time %1, which is <= last output time %2.")
+						.arg(tOutputNext).arg(tOutput), FUNC_ID);
+
+				tOutput = tOutputNext;
+
+				//// if t_outNext is > t_end (including some rounding error compensation), we break the loop
+				//if (tOutputNext > tCommunicationIntervalEnd*(1+1e-10))
+				//	break;
+			} // while (tOutput <= t) {
+
+		} // while (t < tCommunicationIntervalEnd)
 
 		// only for forward time steppping: otherwise in terminate
 		m_model.completeCommunicationInterval();
