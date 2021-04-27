57d166b7f3fdf510dff3de274aca2f33f0b5c32c
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-04-27 13:38:17 +0200

    Now surfaces, rooms, building levels and buildings can be deleted (when empty) (closes #216)

--- SIM-VICUS/src/actions/SVUndoDeleteSelected.h
+++ SIM-VICUS/src/actions/SVUndoDeleteSelected.h
@@ -5,47 +5,28 @@
 
 #include "SVUndoCommandBase.h"
 
-/*! An undo action for deleting selected geometric shapes (things that are shown in the scene).
-	Currently, these are surfaces, anonymous planes, edges, network nodes.
-
-	This UNDO action is a little tricky, since we identify removed surfaces by their unique ID.
-	Hence, when we call undo(), we must ensure that the re-added objects will get the same IDs back.
-	This is done by creating copies of the objects to be removed and storing them in lists.
-	Then, during undo we re-add those objects, hereby keeping their unique IDs.
-*/
+/*! An undo action for deleting selected geometric shapes (things that are shown in the scene). */
 class SVUndoDeleteSelected : public SVUndoCommandBase {
 	Q_DECLARE_TR_FUNCTIONS(SVUndoDeleteSelected)
 public:
-	SVUndoDeleteSelected(const QString & label, const std::vector<unsigned int> & selectedIDs,
-						 const std::vector<VICUS::ComponentInstance> & componentIDs);
+	SVUndoDeleteSelected(const QString & label, const std::vector<unsigned int> & objectIDsToBeRemoved);
 
 	virtual void undo();
 	virtual void redo();
 
 private:
-	struct RemovedSurfaceInfo {
-		RemovedSurfaceInfo() {}
-		RemovedSurfaceInfo(unsigned int	parentRoomId, unsigned int insertIdx, const VICUS::Surface & surface) :
-			m_parentRoomId(parentRoomId), m_insertIdx(insertIdx), m_surface(surface)
-		{}
-		/*! The unique ID of the containing room object */
-		unsigned int	m_parentRoomId;
-		/*! The index of the object when inserted in the original vector. */
-		unsigned int	m_insertIdx;
-		/*! The cached object. */
-		VICUS::Surface	m_surface;
-	};
 
 	/*! Stores the unique IDs of all selected items. */
-	const std::vector<unsigned int> m_selectedIDs;
-
-	/*! Vector with planar geometry. */
-	std::vector<RemovedSurfaceInfo>						m_roomGeometry;
-	/*! Vector with planar anonymous geometry. */
-	std::vector<RemovedSurfaceInfo>						m_plainGeometry;
+	const std::set<unsigned int>						m_selectedIDs;
 
 	/*! Stores vector of component instances. */
 	std::vector<VICUS::ComponentInstance>				m_compInstances;
+	/*! Stores vector of buildings. */
+	std::vector<VICUS::Building>						m_buildings;
+	/*! Stores vector of anonymous surfaces. */
+	std::vector<VICUS::Surface>							m_plainGeometry;
+
+	// TODO Hauke : add network nodes/edges here
 };
 
 
