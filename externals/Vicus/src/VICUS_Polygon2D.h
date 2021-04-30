ccd3b644041dd0cfedde8ea24f6f770744e3de2a
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-04-30 10:53:39 +0200

    Revised Polygon2D code.

--- externals/Vicus/src/VICUS_Polygon2D.h
+++ externals/Vicus/src/VICUS_Polygon2D.h
@@ -1,139 +1,85 @@
 #ifndef VICUS_Polygon2DH
 #define VICUS_Polygon2DH
 
-#include <QVector3D>
-#include <QPolygonF>
+#include <vector>
+#include <IBKMK_Vector2D.h>
 
 #include "VICUS_CodeGenMacros.h"
 
 namespace VICUS {
 
-/*! Class Polygon2D encapsulates the vertex data and plane type of a single plane (2D)
-	in the geometrical model.
-
-	Also, it implements intersection tests (for picking).
+/*! Class Polygon2D stores a polygon of 2D points.
+	Also provides utility functions for checking and simplifying polygon. The data structure ensures that the
+	polygon itselfs is always consistent, i.e. if isValid() returns true, it is guarantied to be non-winding and without
+	consecutive colinear or identical points. Therefore, the polygon can be triangulated right away.
 */
-
-class Polygon2D
-{
+class Polygon2D {
 public:
 
-	/*! Different types of the plane. */
+	/*! Different types of the plane described by the polygon. */
 	enum type_t {
 		/*! Triangle defined through three vertices a, b, c. Triangulation is trivial. */
-		T_Triangle,			// Keyword: Triangle
+		T_Triangle,
 		/*! Rectangle/Parallelogram defined through four vertices (a, b, c and d), where c = a + (b-a) + (d-a).
 			Triangulation gives two triangles.
 		*/
-		T_Rectangle,		// Keyword: Rectangle
+		T_Rectangle,
 		/*! Polygon, generic polygon with n points. */
-		T_Polygon,			// Keyword: Polygon
+		T_Polygon,
 		NUM_T
 	};
 
 
-	/*! Simple storage member to hold vertex indexes of a single triangle.
-		\sa triangles()
-	*/
-	struct triangle_t {
-		triangle_t() {}
-		triangle_t(unsigned short i1, unsigned short i2, unsigned short i3) :
-			a(i1), b(i2), c(i3)
-		{}
-		unsigned short a,b,c;
-	};
-
 	// *** PUBLIC MEMBER FUNCTIONS ***
-
-	/*! Default constructor. */
-	Polygon2D() {}
-	/*! Default constructor. */
-	explicit Polygon2D(type_t t) : m_type(t) {}
-
-//	/*! Initializing constructor.
-//		Vertexes a, b and c must be given in counter-clockwise order, so that (b-a) x (c-a) yields the normal vector of the plane.
-//	*/
-//	PlaneGeometry(type_t t, const IBKMK::Vector3D & a, const IBKMK::Vector3D & b, const IBKMK::Vector3D & c);
-
 
 	void readXML(const TiXmlElement * element);
 	TiXmlElement * writeXML(TiXmlElement * parent) const;
 	VICUS_COMP(Polygon2D)
 
+	/*! Returns the type of the polygon (can be used to optimize some algorithms). */
 	type_t type() const { return m_type; }
+
 	/*! A polygon is considered "fully valid" for painting and additing to the data structure, if
 		it has enough vertexes and can be correctly triangulated (triangles not empty).
 	*/
-	bool isValid() const { return m_vertexes.size() >= 3 && !m_triangles.empty(); }
+	bool isValid() const { return m_valid; }
 
-	/*! Adds a new 2D vertex in the plane of the polygon.
-		Calculates 3D vertex coordinates.
+	/*! Adds a new 2D vertex in the plane of the polygon. Afterwards simplifies polygon. */
+	void addVertex(const IBK::point2D<double> & v);
+
+	/*! Removes the vertex at given location.
+		\warning Throws an exception if index is out of range.
 	*/
-	void addVertex(const QPointF & v);
-
-//	/*! Adds a new 3D vertex.
-//		Calculates 2D plane coordinates and throws an exception, if vertex is out of plane.
-//	*/
-//	void addVertex(const IBKMK::Vector3D & v);
-
-	/*! Removes the vertex at given location. */
 	void removeVertex(unsigned int idx);
-
-	/*! This computes the normal vector, performs the triangulation and attempts to simplify a polygon to a rectangle/triangle
-		primitive.
-		This function is called automatically from readXML().
-	*/
-	void computeGeometry();
 
 	/*! Inverts vertexes so that normal vector is inverted/flipped. */
 	void flip();
 
-	/*! Check for intersection of each edge with the line(p1, p2).
-Intersection point*/
-	bool intersectsLine2D(const QPointF &p1, const QPointF &p2,
-						QPointF & intersectionPoint) const;
-
-	/*! Returns current vector of triangles. */
-	const std::vector<triangle_t> & triangles() const { return m_triangles; }
+	/*! Check for intersection of each edge with the line(p1, p2). Returns true if intersection was found and in this
+		case stores the computed intersection point.
+	*/
+	bool intersectsLine2D(const IBK::point2D<double> &p1, const IBK::point2D<double> &p2, IBK::point2D<double> & intersectionPoint) const;
 
 	/*! Returns 3D vertex coordinates. */
-	std::vector<QPointF> vertexes() const{ return std::vector<QPointF> (m_vertexes.toStdVector());}
+	std::vector<IBKMK::Vector2D> vertexes() const { return m_vertexes; }
 
 	/*! Calculates surface area in m2. */
 	double area() const;
 
-//	/*! Calculates the center point of the surface */
-//	IBKMK::Vector3D centerPoint() const;
-
-//	void setVertexes(const std::vector<IBKMK::Vector3D> & vertexes);
-
-	/*! Returns the 2D polygon (only if it exists). */
-	const QPolygonF & polygon() const { return m_vertexes; }
-
-//	/*! Returns the x-vector of the local coordinate system. */
-//	const IBKMK::Vector3D & localX() const { return m_localX; }
-
-//	/*! Returns the y-vector of the local coordinate system. */
-//	const IBKMK::Vector3D & localY() const { return m_localY; }
-
-
-
-//	/*! Comparison operator == */
-//	bool operator==(const Polygon2D &other) const {return !(*this != other);}
-
 private:
+
 	// *** PRIVATE MEMBER FUNCTIONS ***
 
-	/*! Attempts to convert a polygon geometry to Rectangle type, if the polygon has exactly 4 vertexes and
-		vertex #3 can be constructed from adding (v2-v1) and (v4-v1) to v1 (with some small rounding error tolerance).
-		If the polygon cannot be converted, nothing happens.
+	/*! This function checks the polygon for validity. This function is called automatically from readXML() and
+		from addVertex() and removeVertex().
 	*/
-	void simplify();
+	void checkPolygon();
 
-	/*! This function triangulates the geometry and populate the m_triangles vector.
-		This function is called from computeGeometry().
+	/*! Detects if a polygon geometry with 4 vertices is actually a Rectangle (if the polygon has exactly 4 vertexes and
+		vertex #3 can be constructed from adding (v2-v1) and (v4-v1) to v1 with some small rounding error tolerance).
+		Polyons with 3 vertexes are Triangles. All others are generic polygons.
 	*/
-	void triangulate();
+	void detectType();
 
 	/*! A simple polygon is a polygon without intersects by itself.
 		return true if no intersections
@@ -148,27 +94,19 @@ private:
 
 	/*! Type of the plane.
 		T_POLYGON is the most generic, yet T_TRIANGLE and T_RECTANGLE offer some specialized handling for
-		intersection calculation and data transfer to the graphics pipeline.
+		intersection calculation and triangulation.
 	*/
 	type_t								m_type = NUM_T;
 
 	/*! Points of polyline (in double-precision accuracy!).
 		\warning Do not write to this variable, unless you know what you are doing. Rather use addVertex().
 	*/
-	QPolygonF							m_vertexes;
+	std::vector<IBKMK::Vector2D>		m_vertexes;
 
-	// *** Runtime Variables ***
-
-	/*! Contains the vertex indexes for each triangle that the polygon is composed of (in anti-clock-wise order, so
-		that (b-a) x (c-a) gives the normal vector of the plane.
-		This vector is updated in computeGeometry()/triangulate().
-	*/
-	std::vector<triangle_t>				m_triangles;
-
-private:
-	void readXMLPrivate(const TiXmlElement * element);
-	TiXmlElement * writeXMLPrivate(TiXmlElement * parent) const;
+	/*! Stores the valid state of the polygon, update in checkPolygon() */
+	bool								m_valid = false;
 };
-}
+
+} // namespace VICUS
 
 #endif // VICUS_Polygon2DH
