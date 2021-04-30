7367862a27b51bba8cb65c35b50e7c868e68c5c0
Arombolosh <d-weiss.mail@gmx.de>
2021-04-30 13:35:43 +0200

    add Window and WindowGlazingSystem cpp

---
+++ externals/Vicus/src/VICUS_Window.cpp
@@ -0,0 +1,30 @@
+#include "VICUS_Window.h"
+#include "VICUS_KeywordList.h"
+
+
+namespace VICUS {
+
+AbstractDBElement::ComparisonResult Window::equal(const AbstractDBElement *other) const {
+	const Window * otherWin = dynamic_cast<const Window*>(other);
+	if (otherWin == nullptr)
+		return Different;
+
+	//first check critical data
+
+	//check parameters
+	if(m_glazingSystemID != otherWin->m_glazingSystemID ||
+			m_frame != otherWin->m_frame ||
+			m_divider != otherWin->m_divider)
+		return Different;
+
+	//check meta data
+
+	if(m_displayName != otherWin->m_displayName ||
+			m_dataSource != otherWin->m_dataSource ||
+			m_notes != otherWin->m_notes)
+		return OnlyMetaDataDiffers;
+
+	return Equal;
+}
+
+} // namespace VICUS
