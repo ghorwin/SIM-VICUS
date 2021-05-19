14f6adc8b311b549cd5c62c846234841bb5907aa
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-05-19 12:12:35 +0200

    Windows build scripts updated.

--- build/cmake/build_VC_x64.bat
+++ build/cmake/build_VC_x64.bat
@@ -37,6 +37,7 @@ popd
 xcopy /Y .\bb_VC_x64\View3D\View3D.exe ..\..\bin\release_x64
 xcopy /Y .\bb_VC_x64\NandradSolver\NandradSolver.exe ..\..\bin\release_x64
 xcopy /Y .\bb_VC_x64\SIM-VICUS\SIM-VICUS.exe ..\..\bin\release_x64
+xcopy /Y .\bb_VC_x64\NandradFMUGenerator\NandradFMUGenerator.exe ..\..\bin\release_x64
 
 exit /b 0
 
