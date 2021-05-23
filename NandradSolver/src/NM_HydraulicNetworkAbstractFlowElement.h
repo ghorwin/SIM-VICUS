c7ebcd982de74e2073d4daa0b4645b015624b446
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-05-23 16:43:47 +0200

    License header updated/added where missing.

--- NandradSolver/src/NM_HydraulicNetworkAbstractFlowElement.h
+++ NandradSolver/src/NM_HydraulicNetworkAbstractFlowElement.h
@@ -1,5 +1,26 @@
-#ifndef HydraulicNetworkAbstractFlowElementH
-#define HydraulicNetworkAbstractFlowElementH
+/*	NANDRAD Solver Framework and Model Implementation.
+
+	Copyright (c) 2012-today, Institut für Bauklimatik, TU Dresden, Germany
+
+	Primary authors:
+	  Andreas Nicolai  <andreas.nicolai -[at]- tu-dresden.de>
+	  Anne Paepcke     <anne.paepcke -[at]- tu-dresden.de>
+
+	This program is part of SIM-VICUS (https://github.com/ghorwin/SIM-VICUS)
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
+#ifndef NM_HydraulicNetworkAbstractFlowElementH
+#define NM_HydraulicNetworkAbstractFlowElementH
 
 namespace NANDRAD_MODEL {
 
@@ -29,4 +50,4 @@ public:
 
 } // namespace NANDRAD_MODEL
 
-#endif // HydraulicNetworkAbstractFlowElementH
+#endif // NM_HydraulicNetworkAbstractFlowElementH
--- NandradSolver/src/NM_ThermalComfortModel.h
+++ NandradSolver/src/NM_ThermalComfortModel.h
@@ -1,3 +1,24 @@
+/*	NANDRAD Solver Framework and Model Implementation.
+
+	Copyright (c) 2012-today, Institut für Bauklimatik, TU Dresden, Germany
+
+	Primary authors:
+	  Andreas Nicolai  <andreas.nicolai -[at]- tu-dresden.de>
+	  Anne Paepcke     <anne.paepcke -[at]- tu-dresden.de>
+
+	This program is part of SIM-VICUS (https://github.com/ghorwin/SIM-VICUS)
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
 #ifndef NM_ThermalComfortModelH
 #define NM_ThermalComfortModelH
 
