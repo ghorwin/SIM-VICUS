b2e61659a9ed1c0734ae9a27ec0309741feeb405
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-05-23 16:39:26 +0200

    License header updated/added where missing.

--- externals/Nandrad/src/NANDRAD_ControlElement.h
+++ externals/Nandrad/src/NANDRAD_ControlElement.h
@@ -1,5 +1,26 @@
-#ifndef CONTROLELEMENT_H
-#define CONTROLELEMENT_H
+/*	The NANDRAD data model library.
+
+	Copyright (c) 2012-today, Institut für Bauklimatik, TU Dresden, Germany
+
+	Primary authors:
+	  Andreas Nicolai  <andreas.nicolai -[at]- tu-dresden.de>
+	  Anne Paepcke     <anne.paepcke -[at]- tu-dresden.de>
+
+	This library is part of SIM-VICUS (https://github.com/ghorwin/SIM-VICUS)
+
+	This library is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
+#ifndef NANDRAD_ControlElementH
+#define NANDRAD_ControlElementH
 
 #include "NANDRAD_LinearSplineParameter.h"
 #include "NANDRAD_Constants.h"
@@ -49,4 +70,4 @@ public:
 
 } // namespace NANDRAD
 
-#endif // CONTROLELEMENT_H
+#endif // NANDRAD_ControlElementH
--- externals/Nandrad/src/NANDRAD_HydraulicNetworkHeatExchange.h
+++ externals/Nandrad/src/NANDRAD_HydraulicNetworkHeatExchange.h
@@ -1,5 +1,26 @@
-#ifndef HydraulicNetworkHeatExchangeH
-#define HydraulicNetworkHeatExchangeH
+/*	The NANDRAD data model library.
+
+	Copyright (c) 2012-today, Institut für Bauklimatik, TU Dresden, Germany
+
+	Primary authors:
+	  Andreas Nicolai  <andreas.nicolai -[at]- tu-dresden.de>
+	  Anne Paepcke     <anne.paepcke -[at]- tu-dresden.de>
+
+	This library is part of SIM-VICUS (https://github.com/ghorwin/SIM-VICUS)
+
+	This library is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
+#ifndef NANDRAD_HydraulicNetworkHeatExchangeH
+#define NANDRAD_HydraulicNetworkHeatExchangeH
 
 #include "NANDRAD_LinearSplineParameter.h"
 #include "NANDRAD_HydraulicNetworkComponent.h"
@@ -98,4 +119,4 @@ public:
 
 } // namespace NANDRAD
 
-#endif // HydraulicNetworkHeatExchangeH
+#endif // NANDRAD_HydraulicNetworkHeatExchangeH
