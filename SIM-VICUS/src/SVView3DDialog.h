814c2952693b7e09489404f2796c4880f4971365
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-05-23 17:21:10 +0200

    License header update in SIM-VICUS

--- SIM-VICUS/src/SVView3DDialog.h
+++ SIM-VICUS/src/SVView3DDialog.h
@@ -1,5 +1,30 @@
-#ifndef SVView3DH
-#define SVView3DH
+/*	SIM-VICUS - Building and District Energy Simulation Tool.
+
+	Copyright (c) 2020-today, Institut für Bauklimatik, TU Dresden, Germany
+
+	Primary authors:
+	  Andreas Nicolai  <andreas.nicolai -[at]- tu-dresden.de>
+	  Dirk Weiss  <dirk.weiss -[at]- tu-dresden.de>
+	  Stephan Hirth  <stephan.hirth -[at]- tu-dresden.de>
+	  Hauke Hirsch  <hauke.hirsch -[at]- tu-dresden.de>
+
+	  ... all the others from the SIM-VICUS team ... :-)
+
+	This program is part of SIM-VICUS (https://github.com/ghorwin/SIM-VICUS)
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
+#ifndef SVView3DDialogH
+#define SVView3DDialogH
 
 #include <vector>
 
@@ -128,4 +153,4 @@ public:
 
 };
 
-#endif // SVView3DH
+#endif // SVView3DDialogH
