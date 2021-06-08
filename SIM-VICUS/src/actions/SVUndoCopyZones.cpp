77b2d3fdf3e44a0e426446ed15b6769c8e56cd70
Stephan <stephan.hirth@tu-dresden.de>
2021-06-08 14:49:24 +0200

    Modified Undo Copy Rooms functions

--- SIM-VICUS/src/actions/SVUndoCopyZones.cpp
+++ SIM-VICUS/src/actions/SVUndoCopyZones.cpp
@@ -28,15 +28,17 @@
 
 #include <VICUS_Project.h>
 
-SVUndoCopyZones::SVUndoCopyZones(const QString & label, unsigned int buildingLevelUUID,
-							   const std::vector<VICUS::Room> copiedRooms,
-							   const std::vector<VICUS::ComponentInstance> * componentInstances) :
+SVUndoCopyZones::SVUndoCopyZones(const QString & label,
+								const std::vector<VICUS::Room> copiedRooms,
+								const std::set<unsigned int> deselectedRoomUniqueIDs,
+								const std::vector<VICUS::ComponentInstance> componentInstances,
+								const std::vector<VICUS::SubSurfaceComponentInstance> subSurfacecomponentInstances) :
 	m_copiedRooms(copiedRooms),
-	m_buildingLevelUUID(buildingLevelUUID)
+	m_deselectedUniqueIDs(deselectedRoomUniqueIDs),
+	m_newComponentInstances(componentInstances),
+	m_newSubSurfaceComponentInstances(subSurfacecomponentInstances)
 {
 	setText( label );
-	if (componentInstances != nullptr)
-		m_componentInstances = *componentInstances;
 }
 
 
@@ -53,9 +55,28 @@ void SVUndoCopyZones::undo() {
 		const_cast<VICUS::BuildingLevel *>(bl)->m_rooms.pop_back();
 	}
 
+	// re-select surfaces that were previously selected
+	for (VICUS::Building & b : theProject().m_buildings)
+		for (VICUS::BuildingLevel & bl : b.m_buildingLevels)
+			for (VICUS::Room & r : bl.m_rooms) {
+				if (m_deselectedUniqueIDs.find(r.uniqueID()) != m_deselectedUniqueIDs.end())
+					r.m_selected = true;
+				for (VICUS::Surface & s : r.m_surfaces) {
+						if (m_deselectedUniqueIDs.find(s.uniqueID()) != m_deselectedUniqueIDs.end())
+							s.m_selected = true;
+				}
+			}
+
 	// remove appended component instances (if any)
-//	Q_ASSERT(theProject().m_componentInstances.size() >= m_componentInstances.size());
-//	theProject().m_componentInstances.resize(theProject().m_componentInstances.size() - m_componentInstances.size());
+	Q_ASSERT(theProject().m_componentInstances.size() >= m_newComponentInstances.size());
+	theProject().m_componentInstances.resize(theProject().m_componentInstances.size() - m_newComponentInstances.size());
+
+	// remove appended sub-surface component instances (if any)
+	Q_ASSERT(theProject().m_subSurfaceComponentInstances.size() >= m_newSubSurfaceComponentInstances.size());
+	theProject().m_subSurfaceComponentInstances.resize(theProject().m_subSurfaceComponentInstances.size() - m_newSubSurfaceComponentInstances.size());
+
+	// update pointers
+	theProject().updatePointers();
 
 	// tell project that the geometry has changed (i.e. rebuild navigation tree and scene)
 	SVProjectHandler::instance().setModified( SVProjectHandler::BuildingGeometryChanged);
@@ -74,11 +95,31 @@ void SVUndoCopyZones::redo() {
 
 		// remove last building level
 		Q_ASSERT(!bl->m_rooms.empty());
+
 		const_cast<VICUS::BuildingLevel *>(bl)->m_rooms.push_back(r);
 	}
 
+	// de-select surfaces that were previously selected
+	for (VICUS::Building & b : theProject().m_buildings)
+		for (VICUS::BuildingLevel & bl : b.m_buildingLevels)
+			for (VICUS::Room & r : bl.m_rooms) {
+				if (m_deselectedUniqueIDs.find(r.uniqueID()) != m_deselectedUniqueIDs.end())
+					r.m_selected = false;
+				for (VICUS::Surface & s : r.m_surfaces) {
+						if (m_deselectedUniqueIDs.find(s.uniqueID()) != m_deselectedUniqueIDs.end())
+							s.m_selected = false;
+						for (const VICUS::SubSurface & subS : s.subSurfaces() )
+							const_cast<VICUS::SubSurface &>(subS).m_selected = false;
+				}
+			}
+
+
 	// append component instances (if vector is empty, nothing happens here)
-//	theProject().m_componentInstances.insert(theProject().m_componentInstances.end(), m_componentInstances.begin(), m_componentInstances.end());
+	theProject().m_componentInstances.insert(theProject().m_componentInstances.end(),
+										 m_newComponentInstances.begin(), m_newComponentInstances.end());
+	// append component instances (if vector is empty, nothing happens here)
+	theProject().m_subSurfaceComponentInstances.insert(theProject().m_subSurfaceComponentInstances.end(),
+										 m_newSubSurfaceComponentInstances.begin(), m_newSubSurfaceComponentInstances.end());
 	theProject().updatePointers();
 
 	// tell project that the building geometry has changed
