f5eee411a312907b8c96a2e93b7bd259a1ed7b56
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-07-19 14:13:43 +0200

    Improved jacdump script

--- bin/release/jacdump.sh
+++ bin/release/jacdump.sh
@@ -3,3 +3,10 @@
 NandradSolverJacDump $1
 NandradSolverJacDump $1 --les-solver=Dense
 
+basename=$1
+basename=${basename%.nandrad}
+mv jacobian_dense_cvode.txt ${basename}_jacobian_dense.txt
+mv jacobian_sparse.bin ${basename}_jacobian_sparse.bin
+
+echo 'Created '${basename}_jacobian_dense.txt
+echo 'Created '${basename}_jacobian_sparse.bin
