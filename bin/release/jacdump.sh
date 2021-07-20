90f08e1bd391c4bdee0c74803e45d1a2368796ce
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-07-20 18:56:41 +0200

    Fixed Jacdump script.

--- bin/release/jacdump.sh
+++ bin/release/jacdump.sh
@@ -5,8 +5,8 @@ NandradSolverJacDump $1 --les-solver=Dense --integrator=ImplicitEuler
 
 basename=$1
 basename=${basename%.nandrad}
-mv jacobian_dense_cvode.txt ${basename}_jacobian_dense.txt
+mv jacobian_dense.bin ${basename}_jacobian_dense.bin
 mv jacobian_sparse.bin ${basename}_jacobian_sparse.bin
 
-echo 'Created '${basename}_jacobian_dense.txt
+echo 'Created '${basename}_jacobian_dense.bin
 echo 'Created '${basename}_jacobian_sparse.bin
