145c24c65e3f512e8a3ff7340123ad870d8b843c
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-07-28 11:03:40 +0200

    Documentation updated/fixed.

--- externals/Nandrad/src/NANDRAD_LinearSplineParameter.h
+++ externals/Nandrad/src/NANDRAD_LinearSplineParameter.h
@@ -122,9 +122,11 @@ public:
 	/*! Whether to wrap time around in multi-year simulations (cyclic use) or to assume continuous data. */
 	wrapMethod_t			m_wrapMethod = NUM_C;
 	/*! Data vectors including linear spline functionality (i.e. interpolation at any given value).
-		Values are stored in the respective Base-SI units of the input/output units m_xUnit and
-		m_yUnit. For example, if m_yUnit is 'C' (degree C), then the spline holds values in
-		'K' (degree Kelvin). The functions readXML() and writeXML() perform the necessary conversions.
+		When this class is used as data container, the x and y values are stored in the units m_xUnit and m_yUnit.
+		However, after the call to checkAndInitialize(), the values are stored in the respective Base-SI units
+		of the input/output units m_xUnit and m_yUnit. For example, if m_yUnit is 'C' (degree C), then the spline
+		holds values in 'K' (degree Kelvin).
+		\warning Do not call checkAndInitialize() on original data containers - always create a copy first!
 	*/
 	IBK::LinearSpline		m_values;
 	/*! Unit of the x-values. */
