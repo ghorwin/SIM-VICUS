db85c3be31edb6f3c485737e094c3bf1794afd12
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-08-01 11:53:12 +0200

    Removed CMake warnings on Windows, added illustration image. Updated run_tests.bat script on Windows.

--- build/cmake/run_tests.bat
+++ build/cmake/run_tests.bat
@@ -6,7 +6,7 @@ pushd ..\..\data\tests
 set OLD_NUM_THREADS=%OMP_NUM_THREADS%
 
 set OMP_NUM_THREADS=1
-python ..\..\scripts\TestSuite\run_tests.py -p . -s ..\..\bin\release_x64\NandradSolver -e nandrad
+python ..\..\scripts\TestSuite\run_tests.py -p . -s ..\..\bin\release_x64\NandradSolver -e nandrad --run-all
 set OMP_NUM_THREADS=%OLD_NUM_THREADS%
 
 popd
