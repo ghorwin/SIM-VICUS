caa81f4e3611b9855b29209b7d7247f5dfeeb787
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-08-29 22:18:23 +0200

    Fixed linking issue on Windows, updated CreatDeploy script.

--- bin/release_x64/CreateDeploy_VC2019.bat
+++ bin/release_x64/CreateDeploy_VC2019.bat
@@ -1,6 +1,6 @@
 @echo off
 set QTDIR=C:\Qt\5.15.2\msvc2019_64
-set PATH=C:\Qt\5.15.2\msvc2019_64;%PATH%
+set PATH=c:\Qt\5.15.2\msvc2019_64\bin;%PATH%
 
 :: setup VC environment variables
 set VCVARSALL_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
