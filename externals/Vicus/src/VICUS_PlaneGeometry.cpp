ccd4aea22d4168092f07aaeb2df3a70d34f5f66a
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-09-03 06:57:00 +0200

    Moved QVector3D related calculation routines to IBKMK_2DCalculations for porting (once needed)

--- externals/Vicus/src/VICUS_PlaneGeometry.cpp
+++ externals/Vicus/src/VICUS_PlaneGeometry.cpp
@@ -96,67 +96,6 @@ void PlaneGeometry::removeVertex(unsigned int idx) {
 	triangulate(); // if we have a triangle/rectangle, this is detected here
 }
 
-// TODO : Dirk, move the 2 functions below to IBKMK_2DCalculations and port them to
-//        use Vector2D or point2D
-
-/*!
-	Copyright 2000 softSurfer, 2012 Dan Sunday
-	This code may be freely used and modified for any purpose
-	providing that this copyright notice is included with it.
-	SoftSurfer makes no warranty for this code, and cannot be held
-	liable for any real or imagined damage resulting from its use.
-	Users of this code must verify correctness for their application.
-
-	 isLeft(): tests if a point is Left|On|Right of an infinite line.
-		Input:  three points P0, P1, and P2
-		Return: >0 for P2 left of the line through P0 and P1
-				=0 for P2  on the line
-				<0 for P2  right of the line
-		See: Algorithm 1 "Area of Triangles and Polygons"
-	*/
-
-
-inline int isLeft( QPoint P0, QPoint P1, QPoint P2 )
-{
-	return ( (P1.x() - P0.x()) * (P2.y() - P0.y())
-			- (P2.x() -  P0.x()) * (P1.y() - P0.y()) );
-}
-
-/*!
-	URL: http://geomalgorithms.com/a03-_inclusion.html
-
-	Copyright 2000 softSurfer, 2012 Dan Sunday
-	This code may be freely used and modified for any purpose
-	providing that this copyright notice is included with it.
-	SoftSurfer makes no warranty for this code, and cannot be held
-	liable for any real or imagined damage resulting from its use.
-	Users of this code must verify correctness for their application.
-
-	wn_PnPoly(): winding number test for a point in a polygon
-	  Input:   P = a point,
-			   V[] = vertex points of a polygon V[n+1] with V[n]=V[0]
-	  Return:  wn = the winding number (=0 only when P is outside)
-	*/
-int wn_PnPoly( QPoint P, QPoint *V, int n )
-{
-	int wn = 0;											// the  winding number counter
-
-	// loop through all edges of the polygon
-	for (int i=0; i<n; i++) {							// edge from V[i] to  V[i+1]
-		if (V[i].y() <= P.y()) {						// start y <= P.y
-			if (V[i+1].y()  > P.y())					// an upward crossing
-				 if (isLeft( V[i], V[i+1], P) > 0)		// P left of  edge
-					 ++wn;								// have  a valid up intersect
-		}
-		else {											// start y > P.y (no test needed)
-			if (V[i+1].y()  <= P.y())					// a downward crossing
-				 if (isLeft( V[i], V[i+1], P) < 0)		// P right of  edge
-					 --wn;								// have  a valid down intersect
-		}
-	}
-	return wn;
-}
-
 
 void PlaneGeometry::triangulate() {
 	m_triangulationData.clear();
@@ -225,7 +164,7 @@ void PlaneGeometry::triangulate() {
 		bool valid = true;
 		unsigned int pointCountHole = subSurfacePoly.size();
 		for(unsigned int j=0; j<pointCountHole; ++j) {
-			if (IBKMK::pointInPolygon(parentPoly, subSurfacePoly[j]) == -1){
+			if (IBKMK::pointInPolygon(parentPoly, subSurfacePoly[j]) == -1) {
 				valid = false;
 				break;
 			}
