5318c3cbbe325d7400a4947b1178c9a1a39a18c5
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-11-01 16:42:24 +0100

    Added comment about valueRefs mapping.

--- NandradSolver/src/NM_InternalLoadsModel.cpp
+++ NandradSolver/src/NM_InternalLoadsModel.cpp
@@ -262,7 +262,11 @@ int InternalLoadsModel::update() {
 	for(unsigned int i = 0; i < nZones; ++i) {
 		// retrieve zone area
 		double area = m_zoneAreas[i];
-		// Exactlky 3 values per zone
+		// Exactly 3 values per zone
+		// m_valueRefs has order: [equipLoad_1, personLoad_1, lighting_1,
+		//                         equipLoad_2, personLoad_2, lighting_2,
+		//                         ...
+		//                         equipLoad_n, personLoad_n, lighting_n ]
 		double equipmentLoad = area * (*m_valueRefs[i * 3]);
 		double personLoad = area * (*m_valueRefs[i * 3 + 1]);
 		double lightingLoad = area * (*m_valueRefs[i * 3 + 2]);
