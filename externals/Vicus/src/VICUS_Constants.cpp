c93bfcc1228bf71a572858d119d1318bb5c0e7b3
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-11-29 07:39:29 +0100

    Updated version to 0.6.4

--- externals/Vicus/src/VICUS_Constants.cpp
+++ externals/Vicus/src/VICUS_Constants.cpp
@@ -28,7 +28,7 @@
 namespace VICUS {
 
 const char * const VERSION = "0.6";
-const char * const LONG_VERSION = "0.6.3";
+const char * const LONG_VERSION = "0.6.4";
 
 unsigned int INVALID_ID = 0xFFFFFFFF;
 
