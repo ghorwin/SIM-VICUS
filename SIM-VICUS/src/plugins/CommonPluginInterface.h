b66dfaedb667c6d8ba4043045a262da04d9b35ef
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-12-22 07:40:16 +0100

    Added plugin interface base class and readme.

---
+++ SIM-VICUS/src/plugins/CommonPluginInterface.h
@@ -0,0 +1,41 @@
+#ifndef CommonPluginInterfaceH
+#define CommonPluginInterfaceH
+
+class QWidget;
+
+/*! This class declares common plugin interface functions.
+	This header should be included by all plugin interface classes.
+*/
+class CommonPluginInterface {
+public:
+	/*! Return values for the showSettingsDialog() function. */
+	enum SettingsDialogUpdateNeeds {
+		/*! No update needed. */
+		NoUpdate,
+		/*! Something has changed that affects the display in the scene, 
+			for example colors (maybe component colors).
+		*/
+		SceneUpdate = 0x01,
+		/*! Something has changed related to databases and all widgets
+			showing database content (colors, names, labels) shall be updated.
+		*/
+		DatabaseUpdate = 0x02
+	}
+
+	/*! If this function returns true, the plugin provides a
+		settings/configuration page.
+	*/
+	virtual bool haveSettingsDialog() const { return false; }
+	
+	/*! If a settings dialog page is provided, this function is called when
+		the user clicks on the respective settings dialog menu entry.
+		\param parent Parent class pointer, to be used as parent for modal dialogs.
+		\return Returns a bitmask that signals what kind of update is needed
+			in the user-interface as consequence of the settings dialogs
+			changes (see SettingsDialogUpdateNeeds).
+	*/
+	virtual int showSettingsDialog(QWidget * parent) { return NoUpdate; }
+};
+
+
+#endif // CommonPluginInterfaceH
