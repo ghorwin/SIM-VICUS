830355db0ebb1b56bbaf339b04d762174ce7f5dd
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-12-24 10:05:07 +0100

    Create deploy updated for Windows

--- bin/release_x64/CreateDeploy_VC2019.bat
+++ bin/release_x64/CreateDeploy_VC2019.bat
@@ -4,5 +4,6 @@ set PATH=c:\Qt\5.15.2\msvc2019_64\bin;%PATH%
 
 :: setup VC environment variables
 set VCVARSALL_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+call %VCVARSALL_PATH%
 
 windeployqt.exe SIM-VICUS.exe
