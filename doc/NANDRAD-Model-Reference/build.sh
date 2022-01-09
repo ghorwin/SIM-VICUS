89ed07922e4eca302ca1f9ae633b753fd726c34a
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2022-01-09 18:06:00 +0100

    Updated documentation for binary file format (btf)

--- doc/NANDRAD-Model-Reference/build.sh
+++ doc/NANDRAD-Model-Reference/build.sh
@@ -3,18 +3,18 @@
 ADOC=NANDRAD-Model-Reference
 
 echo '*** Generating html ***' &&
-python ../adoc_utils/scripts/adoc-image-prep.py html . &&
-asciidoctor -a lang=de -r asciidoctor-mathematical $ADOC.adoc &&
+python3 ../adoc_utils/scripts/adoc-image-prep.py html . &&
+asciidoctor -a lang=de $ADOC.adoc &&
 
 echo &&
 echo '*** Generating pdf ***' &&
-python ../adoc_utils/scripts/adoc-image-prep.py pdf . &&
-asciidoctor-pdf -a lang=de  -a pdf-theme=../adoc_utils/pdf-theme.yml -r asciidoctor-mathematical  -r ../adoc_utils/rouge_theme.rb -a pdf-fontsdir="../adoc_utils/fonts;GEM_FONTS_DIR" $ADOC.adoc &&
+python3 ../adoc_utils/scripts/adoc-image-prep.py pdf . &&
+asciidoctor-pdf -a lang=de  -a pdf-theme=../adoc_utils/pdf-theme.yml  -r ../adoc_utils/rouge_theme.rb -a pdf-fontsdir="../adoc_utils/fonts;GEM_FONTS_DIR" $ADOC.adoc &&
 
 # restore html-type image files
 echo &&
 echo '*** Restoring html ***' &&
-python ../adoc_utils/scripts/adoc-image-prep.py html . &&
+python3 ../adoc_utils/scripts/adoc-image-prep.py html . &&
 
 echo &&
 echo '*** Copying files to ../../docs directory' &&
