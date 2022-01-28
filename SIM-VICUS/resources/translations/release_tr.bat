9327368d4c1a1649ac2aa2c871f513822761699d
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2022-01-28 20:44:56 +0100

    Added initial version of german translation file

--- SIM-VICUS/resources/translations/release_tr.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PATH=c:\Qt\5.11.3\msvc2015_64\bin;%PATH%
-lrelease SIM-VICUS_de.ts -qm SIM-VICUS_de.qm
-pause
-
--- SIM-VICUS/resources/translations/update_tr.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set PATH=c:\Qt\5.11.3\msvc2015_64\bin;%PATH%
-lupdate ..\..\projects\Qt\SIM-VICUS.pro
-
-pause
-
-linguist SIM-VICUS_de.ts
-
