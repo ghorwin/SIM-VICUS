f1240483cfbf974085c35e09efdd3e6f99b92d95
Arombolosh <d-weiss.mail@gmx.de>
2022-02-17 11:09:52 +0100

    bugfix idf import

--- externals/IDFReader/src/EP_Zone.cpp
+++ externals/IDFReader/src/EP_Zone.cpp
@@ -27,12 +27,12 @@ void Zone::read(const std::vector<std::string> & str, unsigned int /*version*/) 
 	else
 		m_ceilingHeight = -1;
 
-	if (!str[8].empty() && IBK::tolower_string(str[9]) != "autocalculate")
+	if (!str[9].empty() && IBK::tolower_string(str[9]) != "autocalculate")
 		m_volume = IBK::string2val<double>(str[9]);
 	else
 		m_volume = -1;
 
-	if (!str[8].empty() && IBK::tolower_string(str[10]) != "autocalculate")
+	if (!str[10].empty() && IBK::tolower_string(str[10]) != "autocalculate")
 		m_floorArea = IBK::string2val<double>(str[10]);
 	else
 		m_floorArea = -1;
