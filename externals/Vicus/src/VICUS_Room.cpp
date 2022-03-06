543eee7c908391a3786c3ba719fdc8a39ed23b1d
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2022-03-06 16:11:25 +0100

    Room area/volumen calculation refactoring

--- externals/Vicus/src/VICUS_Room.cpp
+++ externals/Vicus/src/VICUS_Room.cpp
@@ -32,34 +32,27 @@ namespace VICUS {
 
 void Room::calculateFloorArea() {
 	double floorarea = 0;
-	for(const VICUS::Surface & s : m_surfaces){
-		Polygon3D poly = s.polygon3D();
+	for (const VICUS::Surface & s : m_surfaces){
 		// include all surfaces which normal a quiet similar to -Z normal (5 DEG deviation)
 		double cosAlpha = std::cos(5*IBK::DEG2RAD);
-		double scalarProduct = poly.normal().scalarProduct(IBKMK::Vector3D(0,0,-1));
-		if(scalarProduct >= cosAlpha )
+		double scalarProduct = s.geometry().normal().scalarProduct(IBKMK::Vector3D(0,0,-1));
+		if (scalarProduct >= cosAlpha )
 			floorarea += s.geometry().area(2);
 	}
 	VICUS::KeywordList::setParameter(m_para,"Room::para_t", VICUS::Room::P_Area, floorarea);
 }
 
+
 void Room::calculateVolume() {
 	double vol = 0;
-	for(const VICUS::Surface & s : m_surfaces){
+	// process all surfaces
+	for (const VICUS::Surface & s : m_surfaces) {
+		const PlaneTriangulationData & planeTri = s.geometry().triangulationDataWithoutHoles();
 
-		PlaneGeometry pg;
-		// in s.geometry() are only these polygons which are clipped by holes ...
-		// we need all opake surfaces without hole
-		// so triangulate again
-		pg.setPolygon(s.polygon3D());
-
-		const PlaneTriangulationData planeTri = pg.triangulationData();
-//		std::cout << "Surface Name:\t" << s.m_displayName.toStdString() << std::endl;
-
-		// Opaque surfaces (these may contain holes such as windows)
-		for(unsigned int i=0; i<planeTri.m_triangles.size(); ++i){
+		// all triangles
+		for (unsigned int i=0; i<planeTri.m_triangles.size(); ++i) {
 			const IBKMK::Triangulation::triangle_t &tri = planeTri.m_triangles[i];
-			if(tri.isDegenerated())
+			if (tri.isDegenerated())
 				continue;
 			const IBKMK::Vector3D & p0 = planeTri.m_vertexes[tri.i1];
 			const IBKMK::Vector3D & p1 = planeTri.m_vertexes[tri.i2];
@@ -67,22 +60,12 @@ void Room::calculateVolume() {
 
 			vol += (( p1.m_y - p0.m_y ) * ( p2.m_z - p0.m_z ) - ( p1.m_z - p0.m_z ) * ( p2.m_y - p0.m_y ))
 					* ( p0.m_x + p1.m_x + p2.m_x );
-
-			// ToDo Anne: Das hier bitte prüfen
-//			std::cout << "\tTeil" << i << ":" << std::endl;
-//			std::cout << "\tP0:\t" << p0.m_x << "\t" << p0.m_y << "\t" << p0.m_z <<  "\tNormale:\t" << planeTri.m_normal.m_x << "\t" << planeTri.m_normal.m_y << "\t" << planeTri.m_normal.m_z << std::endl;
-//			std::cout << "\tP1:\t" << p1.m_x << "\t" << p1.m_y << "\t" << p1.m_z << std::endl;
-//			std::cout << "\tP2:\t" << p2.m_x << "\t" << p2.m_y << "\t" << p2.m_z << std::endl;
-
 		}
-
 	}
 
-	vol /= 6; // FIXME: funktioniert bei Dächern nicht!
+	vol /= 6;
 
 	VICUS::KeywordList::setParameter(m_para,"Room::para_t", VICUS::Room::P_Volume, vol);
-
-
 }
 
 
