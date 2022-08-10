534bedc0cc161bee8a6328abe2e9fc501fce0771
hirseboy <stephan.hirth@tu-dresden.de>
2022-08-10 10:13:55 +0200

    Fixed errors in idf import. Projection on plane now working when polygon3D could initially not set any vertexes.

--- SIM-VICUS/src/SVImportIDFDialog.cpp
+++ SIM-VICUS/src/SVImportIDFDialog.cpp
@@ -826,10 +826,11 @@ void SVImportIDFDialog::transferData(const EP::Project & prj, unsigned int start
 		// however, yet without holes
 		VICUS::Polygon3D p(bsd.m_polyline);
 		// TODO : Error handling?
-		surf.setPolygon3D( p );
+		if(!p.polyline().vertexes().empty())
+			surf.setPolygon3D( p );
 
 		// we can only import a subsurface, if the surface itself has a valid polygon
-		if (!surf.geometry().isValid() && surf.polygon3D().vertexes().size() > 2) {
+		if (p.polyline().vertexes().empty() || (!surf.geometry().isValid() && surf.polygon3D().vertexes().size() > 2)) {
 			//			surf.setPolygon3D( VICUS::Polygon3D( bsd.m_polyline ) ); // for debugging purposes - to see, why polygon isn't valid
 			const std::vector<IBKMK::Vector3D> &poly3D = bsd.m_polyline;
 
