34936dadf658bf10df44330843ff177344fb24fc
Stephan Hirth <stephan.hirth@tu-dresden.de>
2022-09-14 18:03:07 +0200

    Edited coloring of validating line edit

--- externals/QtExt/src/QtExt_ValidatingLineEdit.cpp
+++ externals/QtExt/src/QtExt_ValidatingLineEdit.cpp
@@ -191,7 +191,7 @@ void ValidatingLineEdit::onTextChanged ( const QString& ) {
 	}
 	if (isReadOnly()) {
 		QPalette palEdit;
-		setStyleSheet("QLineEdit { background-color: "+ Style::ReadOnlyEditFieldBackground +";}");
+		setStyleSheet("QLineEdit { background-color: "+ Style::ReadOnlyEditFieldBackground +"; color: 'white'}");
 		setToolTip("");
 		return;
 	}
