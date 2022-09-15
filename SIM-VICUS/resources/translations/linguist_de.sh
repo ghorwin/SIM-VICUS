f0210601ef3e80225896fa48ce2c38fac895e263
Arombolosh <d-weiss.mail@gmx.de>
2022-09-15 14:54:55 +0200

    update shell script linguist

--- SIM-VICUS/resources/translations/linguist_de.sh
+++ SIM-VICUS/resources/translations/linguist_de.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH=~/Qt/5.11.3/gcc_64/bin/:$PATH
+export PATH=~/Qt/5.15.2/gcc_64/bin/:$PATH
 
 linguist ../../../externals/QtExt/resources/translations/QtExt_de.ts
 linguist SIM-VICUS_de.ts
