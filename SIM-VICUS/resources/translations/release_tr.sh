e38e9918fe32d948d3a2be10f8e62d903e97a2c2
Arombolosh <d-weiss.mail@gmx.de>
2022-09-16 10:14:37 +0200

    update path in translation release shell (linux)

--- SIM-VICUS/resources/translations/release_tr.sh
+++ SIM-VICUS/resources/translations/release_tr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH=~/Qt/5.11.3/gcc_64/bin/:$PATH
+export PATH=~/Qt/5.15.2/gcc_64/bin/:$PATH
 
 lrelease ../../../externals/QtExt/resources/translations/QtExt_de.ts SIM-VICUS_de.ts -qm SIM-VICUS_de.qm
 
