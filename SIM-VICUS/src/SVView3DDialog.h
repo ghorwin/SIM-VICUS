72f3fa606493995a62e81914e8c87b40e36f01e2
Stephan Hirth <58851829+hirseboy@users.noreply.github.com>
2023-01-09 14:56:57 +0100

    Reenabled view3D Export - ToDo: Data structure for viewing factors needed and import from view3D files.

--- SIM-VICUS/src/SVView3DDialog.h
+++ SIM-VICUS/src/SVView3DDialog.h
@@ -81,7 +81,7 @@ public:
 		{
 		}
 
-		unsigned int					m_id;			///> id of surface
+        unsigned int					m_id;			///> id of surface
 
 		unsigned int					m_vicusId;		///> corresponding vicus id
 
