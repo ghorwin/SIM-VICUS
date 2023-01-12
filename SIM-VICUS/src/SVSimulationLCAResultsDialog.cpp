269e6007c14f6a9752e1cca155c867aacd13c67a
Stephan Hirth <58851829+hirseboy@users.noreply.github.com>
2023-01-12 12:34:08 +0100

    Complete overhaul LCA Results Dialog

--- SIM-VICUS/src/SVSimulationLCAResultsDialog.cpp
+++ SIM-VICUS/src/SVSimulationLCAResultsDialog.cpp
@@ -1,5 +1,12 @@
 #include "SVSimulationLCAResultsDialog.h"
+#include "SVProjectHandler.h"
+#include "SVStyle.h"
+#include "QtExt_Conversions.h"
 #include "ui_SVSimulationLCAResultsDialog.h"
+
+#include <QTreeWidgetItem>
+
+#include <VICUS_KeywordList.h>
 
 SVSimulationLCAResultsDialog::SVSimulationLCAResultsDialog(QWidget *parent) :
 	QDialog(parent),
@@ -14,19 +21,128 @@ SVSimulationLCAResultsDialog::~SVSimulationLCAResultsDialog() {
 	delete m_ui;
 }
 
-void SVSimulationLCAResultsDialog::setLcaResults() {
-	//m_ui->treeWidgetLcaResults->
-	setup();
+void SVSimulationLCAResultsDialog::setLcaResults(const std::map<VICUS::Component::ComponentType, AggregatedComponentData> &lcaResultMap,
+												 const std::map<unsigned int, AggregatedComponentData> compIdToAggregatedData,
+												 const VICUS::EpdDataset::Category &category,
+												 const VICUS::LcaSettings &settings) const {
+	double scaleFactor = 1.0;
+
+	if(category != VICUS::EpdDataset::C_CategoryB)
+		scaleFactor /= settings.m_para[VICUS::LcaSettings::P_TimePeriod].get_value();
+	scaleFactor /= settings.m_para[VICUS::LcaSettings::P_NetUsageArea].get_value();
+	scaleFactor *= settings.m_para[VICUS::LcaSettings::P_FactorBnbSimpleMode].get_value();
+
+	QTreeWidgetItem *rootItem = new QTreeWidgetItem(m_ui->treeWidgetLcaResults);
+	rootItem->setText(ColCategory, VICUS::KeywordList::Description("EpdDataset::Category", category));
+	QFont font;
+	font.setBold(true);
+	rootItem->setFont(ColCategory, font);
+
+	VICUS::EpdCategoryDataset epdDataset;
+
+	for(std::map<VICUS::Component::ComponentType, AggregatedComponentData>::const_iterator itAggregatedComp = lcaResultMap.begin();
+		itAggregatedComp != lcaResultMap.end(); ++itAggregatedComp)
+	{
+
+		QStringList lcaDataType;
+		const AggregatedComponentData &aggregatedTypeData = itAggregatedComp->second;
+
+		std::set<unsigned int> usedCompIds;
+		for(const VICUS::Component *comp : aggregatedTypeData.m_additionalComponents) {
+			if(m_idComponentEpdUndefined.find(comp->m_id) == m_idComponentEpdUndefined.end()) {
+				usedCompIds.insert(comp->m_id);
+			}
+		}
+		if(usedCompIds.empty())
+			continue;
+
+		QTreeWidgetItem *item = new QTreeWidgetItem();
+		rootItem->addChild(item);
+		item->setText(ColComponentType, VICUS::KeywordList::Description("Component::ComponentType", aggregatedTypeData.m_component->m_type));
+		item->setText(ColArea, QString::number(aggregatedTypeData.m_area));
+		item->setBackgroundColor(ColColor, aggregatedTypeData.m_component->m_color);
+
+		VICUS::EpdCategoryDataset epd = aggregatedTypeData.m_totalEpdData[category].calcTotalEpdByCategory(category, SVProjectHandler::instance().project().m_lcaSettings);
+		epdDataset += epd;
+
+		item->setText(ColGWP, QString::number(scaleFactor * epd.m_para[VICUS::EpdCategoryDataset::P_GWP].get_value()));
+		item->setText(ColAP, QString::number(scaleFactor * epd.m_para[VICUS::EpdCategoryDataset::P_AP].get_value()));
+		item->setText(ColEP, QString::number(scaleFactor * epd.m_para[VICUS::EpdCategoryDataset::P_EP].get_value()));
+		item->setText(ColODP, QString::number(scaleFactor * epd.m_para[VICUS::EpdCategoryDataset::P_ODP].get_value()));
+		item->setText(ColPOCP, QString::number(scaleFactor * epd.m_para[VICUS::EpdCategoryDataset::P_POCP].get_value()));
+
+		item->setExpanded(false);
+
+		m_ui->treeWidgetLcaResults->addTopLevelItem(item);
+
+		for(const VICUS::Component *comp : aggregatedTypeData.m_additionalComponents) {
+
+			const AggregatedComponentData &aggregatedCompData = compIdToAggregatedData.at(comp->m_id);
+
+			if(usedCompIds.find(comp->m_id) == usedCompIds.end())
+				continue; // Skip unused ids
+
+			QStringList lcaData;
+			QTreeWidgetItem *itemChild = new QTreeWidgetItem();
+			VICUS::EpdCategoryDataset epdChild = aggregatedCompData.m_totalEpdData[category].calcTotalEpdByCategory(category, SVProjectHandler::instance().project().m_lcaSettings);
+			epdDataset += epdChild;
+
+			itemChild->setText(ColComponentName, QtExt::MultiLangString2QString(comp->m_displayName));
+			itemChild->setText(ColArea, QString::number(aggregatedCompData.m_area));
+			itemChild->setBackgroundColor(ColColor, aggregatedCompData.m_component->m_color);
+
+			itemChild->setText(ColGWP, QString::number(scaleFactor * epd.m_para[VICUS::EpdCategoryDataset::P_GWP].get_value()));
+			itemChild->setText(ColAP, QString::number(scaleFactor * epd.m_para[VICUS::EpdCategoryDataset::P_AP].get_value()));
+			itemChild->setText(ColEP, QString::number(scaleFactor * epd.m_para[VICUS::EpdCategoryDataset::P_EP].get_value()));
+			itemChild->setText(ColODP, QString::number(scaleFactor * epd.m_para[VICUS::EpdCategoryDataset::P_ODP].get_value()));
+			itemChild->setText(ColPOCP, QString::number(scaleFactor * epd.m_para[VICUS::EpdCategoryDataset::P_POCP].get_value()));
+
+			item->addChild(itemChild);
+
+		}
+	}
+
+	rootItem->setText(ColGWP, QString::number(scaleFactor * epdDataset.m_para[VICUS::EpdCategoryDataset::P_GWP].get_value()));
+	rootItem->setText(ColAP, QString::number(scaleFactor * epdDataset.m_para[VICUS::EpdCategoryDataset::P_AP].get_value()));
+	rootItem->setText(ColEP, QString::number(scaleFactor * epdDataset.m_para[VICUS::EpdCategoryDataset::P_EP].get_value()));
+	rootItem->setText(ColODP, QString::number(scaleFactor * epdDataset.m_para[VICUS::EpdCategoryDataset::P_ODP].get_value()));
+	rootItem->setText(ColPOCP, QString::number(scaleFactor * epdDataset.m_para[VICUS::EpdCategoryDataset::P_POCP].get_value()));
+
+	for(unsigned int i=0; i<NumCol; ++i)
+		m_ui->treeWidgetLcaResults->resizeColumnToContents(i);
+
+	m_ui->treeWidgetLcaResults->setColumnWidth(ColColor, 20);
 }
+
 
 void SVSimulationLCAResultsDialog::setup() {
 	// Add data to treeWidget
 	m_ui->treeWidgetLcaResults->clear();
-	m_ui->treeWidgetLcaResults->setColumnCount(15);
+	m_ui->treeWidgetLcaResults->setColumnCount(NumCol);
 	QStringList headers;
-	headers << "Category" << "Component type" << "Component name" << "Area [m2]" << "GWP (CO2-Äqu.) [kg/(m2a)";
+	headers << "Category" << "" << "Component type" << "Component name" << "Area [m2]" << "GWP (CO2-Äqu.) [kg/(m2a)";
 	headers << "ODP (R11-Äqu.) [kg/(m2a)]" << "POCP (C2H4-Äqu.) [kg/(m2a)]" << "AP (SO2-Äqu.) [kg/(m2a)]" << "EP (PO4-Äqu.) [kg/(m2a)]";
 
 	m_ui->treeWidgetLcaResults->setHeaderLabels(headers);
 
+	SVStyle::formatDatabaseTreeView(m_ui->treeWidgetLcaResults);
+
+	m_ui->treeWidgetLcaResults->setItemsExpandable(true);
+
 }
+
+void SVSimulationLCAResultsDialog::on_treeWidgetLcaResults_itemExpanded(QTreeWidgetItem *item) {
+	for(unsigned int i=0; i<NumCol; ++i)
+		m_ui->treeWidgetLcaResults->resizeColumnToContents(i);
+
+	m_ui->treeWidgetLcaResults->setColumnWidth(ColColor, 20);
+}
+
+
+void SVSimulationLCAResultsDialog::on_treeWidgetLcaResults_itemCollapsed(QTreeWidgetItem *item) {
+	for(unsigned int i=0; i<NumCol; ++i)
+		m_ui->treeWidgetLcaResults->resizeColumnToContents(i);
+
+	m_ui->treeWidgetLcaResults->setColumnWidth(ColColor, 20);
+}
+
--- SIM-VICUS/src/SVSimulationLCAResultsDialog.h
+++ SIM-VICUS/src/SVSimulationLCAResultsDialog.h
@@ -1,6 +1,84 @@
 #ifndef SVSimulationLCAResultsDialogH
 #define SVSimulationLCAResultsDialogH
+
 #include <QDialog>
+#include <QTreeWidgetItem>
+
+#include "SVSettings.h"
+
+#include <VICUS_Component.h>
+#include <VICUS_ComponentInstance.h>
+#include <VICUS_EpdDataset.h>
+#include <VICUS_Surface.h>
+
+struct AggregatedComponentData {
+
+	AggregatedComponentData() {}
+
+	/*! Construct AggregatedComponentData with pointer to VICUS::Component and
+		empty Area. */
+	AggregatedComponentData(const VICUS::ComponentInstance &compInst) :
+		m_component(SVSettings::instance().m_db.m_components[compInst.m_idComponent]),
+		m_area(0.0)
+	{
+		addArea(compInst);
+		m_additionalComponents.insert(m_component);
+	}
+
+	/*! Adds area of an component instance to the aggregated component data. */
+	void addArea(const VICUS::ComponentInstance &compInst) {
+		const VICUS::Surface *surfA = compInst.m_sideASurface;
+		const VICUS::Surface *surfB = compInst.m_sideASurface;
+
+		if(surfA == nullptr && surfB == nullptr)
+			return;
+
+		if(surfA != nullptr) {
+			m_area += surfA->geometry().area();
+			for(unsigned int i=0; i<surfA->subSurfaces().size(); ++i) {
+				m_area -= surfA->subSurfaces()[i].m_polygon2D.area();
+			}
+		}
+		else if(surfB != nullptr) {
+			m_area += surfB->geometry().area();
+			for(unsigned int i=0; i<surfB->subSurfaces().size(); ++i) {
+				m_area -= surfA->subSurfaces()[i].m_polygon2D.area();
+			}
+		}
+	}
+
+	/*! Combines Aggregated Component data.
+		Adds Area, Aggregated LCA Data and inserts pointer to additional components.
+	*/
+	void addAggregatedData(const AggregatedComponentData &aggregatedData) {
+
+		m_area += aggregatedData.m_area;
+
+		for(unsigned int i=0; i<VICUS::EpdDataset::NUM_C; ++i) {
+			m_totalEpdData[i] += aggregatedData.m_totalEpdData[i];
+		}
+
+		m_additionalComponents.insert(aggregatedData.m_component);
+	}
+
+	/*! Pointer to VICUS Component. */
+	const VICUS::Component					*m_component = nullptr;
+
+	/*! Vector with additional components. */
+	std::set<const VICUS::Component *>		m_additionalComponents;
+
+	/*! Area of all components used in VICUS project. */
+	double									m_area;
+
+	/*! Global EPD of used Component. */
+	VICUS::EpdDataset						m_totalEpdData[VICUS::EpdDataset::NUM_C];
+
+};
+
+
+namespace VICUS {
+class Component;
+}
 
 namespace Ui {
 class SVSimulationLCAResultsDialog;
@@ -10,17 +88,42 @@ class SVSimulationLCAResultsDialog : public QDialog
 {
 	Q_OBJECT
 
+	enum Cols {
+		ColCategory,
+		ColColor,
+		ColComponentType,
+		ColComponentName,
+		ColArea,
+		ColGWP,
+		ColODP,
+		ColPOCP,
+		ColAP,
+		ColEP,
+		NumCol
+	};
+
 public:
 	explicit SVSimulationLCAResultsDialog(QWidget *parent = nullptr);
 	~SVSimulationLCAResultsDialog();
 
-	void setLcaResults();
+	void setLcaResults(const std::map<VICUS::Component::ComponentType, AggregatedComponentData> &lcaResultMap,
+					   const std::map<unsigned int, AggregatedComponentData> compIdToAggregatedData,
+					   const VICUS::EpdDataset::Category &category, const VICUS::LcaSettings &settings) const;
 
+
+private slots:
+	void on_treeWidgetLcaResults_itemExpanded(QTreeWidgetItem *item);
+
+	void on_treeWidgetLcaResults_itemCollapsed(QTreeWidgetItem *item);
 
 private:
 
 	/*! Sets up Dialog with LCA Data. Needs to be called once before Dialog is beeing used. */
 	void setup();
+
+	/*! Set with ids of components with undefined data, that we have to skip.
+	*/
+	std::set<unsigned int>					m_idComponentEpdUndefined;
 
 	/*! Pointer to UI. */
 	Ui::SVSimulationLCAResultsDialog *m_ui;
