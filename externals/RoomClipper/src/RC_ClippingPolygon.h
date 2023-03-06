ae55f490e8606ffbfae9d87b25dad96c536471cc
Stephan Hirth <stephan.hirth@tu-dresden.de>
2023-03-06 17:06:19 +0100

    Added documentation

--- externals/RoomClipper/src/RC_ClippingPolygon.h
+++ externals/RoomClipper/src/RC_ClippingPolygon.h
@@ -1,3 +1,24 @@
+/*	The RoomClipper data model library.
+
+	Copyright (c) 2012-today, Institut für Bauklimatik, TU Dresden, Germany
+
+	Primary authors:
+	  Stephan Hirth     <stephan.hirth -[at]- tu-dresden.de>
+	  Dirk Weiß         <dirk.weis     -[at]- tu-dresden.de>
+
+	This library is part of SIM-VICUS (https://github.com/ghorwin/SIM-VICUS)
+
+	This library is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
 #ifndef RC_ClippingPolygonH
 #define RC_ClippingPolygonH
 
