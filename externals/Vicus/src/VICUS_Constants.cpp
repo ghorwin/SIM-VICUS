0adfd19dfba0b3f94baeec7bc6e19eb64e88bf15
Hauke <hauke.hirsch@tu-dresden.de>
2023-04-25 20:02:32 +0200

    version number

--- externals/Vicus/src/VICUS_Constants.cpp
+++ externals/Vicus/src/VICUS_Constants.cpp
@@ -28,7 +28,7 @@
 namespace VICUS {
 
 const char * const VERSION = "1.0";
-const char * const LONG_VERSION = "1.0.2";
+const char * const LONG_VERSION = "1.0.3";
 
 unsigned int INVALID_ID = 0xFFFFFFFF;
 
