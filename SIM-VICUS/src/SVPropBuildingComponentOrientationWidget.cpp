140289aed4ce23a6069a84c267735ded49d58c36
Stephan Hirth <stephan.hirth@tu-dresden.de>
2023-04-25 17:39:02 +0200

    Updated prop widgets

--- SIM-VICUS/src/SVPropBuildingComponentOrientationWidget.cpp
+++ SIM-VICUS/src/SVPropBuildingComponentOrientationWidget.cpp
@@ -15,7 +15,7 @@ SVPropBuildingComponentOrientationWidget::SVPropBuildingComponentOrientationWidg
 	m_ui(new Ui::SVPropBuildingComponentOrientationWidget)
 {
 	m_ui->setupUi(this);
-	m_ui->verticalLayout->setMargin(0);
+	m_ui->gridLayout->setMargin(0);
 
 	m_ui->frameSideA->setStyleSheet(".QFrame { background-color: #2f7dd4; }");
 	m_ui->frameSideB->setStyleSheet(".QFrame { background-color: #ffce30; }");
