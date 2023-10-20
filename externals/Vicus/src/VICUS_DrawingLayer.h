faff5fbb6b046b16c80d61cef6d4373ef32b691a
Stephan Hirth <stephan.hirth@tu-dresden.de>
2023-10-20 17:34:49 +0200

    Removed block from drawing layer

--- externals/Vicus/src/VICUS_DrawingLayer.h
+++ externals/Vicus/src/VICUS_DrawingLayer.h
@@ -13,25 +13,6 @@ namespace VICUS {
 class DrawingLayer : public Object {
 public:
 
-	/*! Dummy Class for Blocks */
-	struct Block {
-
-		TiXmlElement * writeXML(TiXmlElement * element) const;
-		void readXML(const TiXmlElement * element);
-
-		/*! ID of Block. */
-		unsigned int	m_id;
-		/*! Name of Block. */
-		QString			m_name;
-		/*! Block color. */
-		QColor			m_color = QColor();
-		/*! Line weight. */
-		int				m_lineWeight;
-		/*! Base point */
-		IBKMK::Vector2D	m_basePoint;
-	};
-
-
 	DrawingLayer() {}
 
 	const char * typeinfo() const override {
@@ -52,11 +33,6 @@ public:
 	int				m_lineWeight;				// XML:A
 	/*! ID of block. */
 	unsigned int	m_idBlock = INVALID_ID;		// XML:A
-
-
-	/*! If Layer belongs to a block, pointer to block is here, else nullptr */
-	Block			*m_block = nullptr;
-
 };
 
 }
