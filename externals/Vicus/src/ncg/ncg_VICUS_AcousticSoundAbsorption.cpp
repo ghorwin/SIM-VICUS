20d69bd7d143d7b2523b5a37a30a4f7f2f892d94
Stephan Hirth <stephan.hirth@tu-dresden.de>
2023-11-01 13:05:28 +0100

    Updated ncgs

--- externals/Vicus/src/ncg/ncg_VICUS_AcousticSoundAbsorption.cpp
+++ externals/Vicus/src/ncg/ncg_VICUS_AcousticSoundAbsorption.cpp
@@ -19,7 +19,7 @@
 	Lesser General Public License for more details.
 */
 
-#include <VICUS_SoundAbsorption.h>
+#include <VICUS_AcousticSoundAbsorption.h>
 #include <VICUS_KeywordList.h>
 
 #include <IBK_messages.h>
@@ -32,10 +32,28 @@
 
 namespace VICUS {
 
-void SoundAbsorption::readXML(const TiXmlElement * element) {
-	FUNCID(SoundAbsorption::readXML);
+void AcousticSoundAbsorption::readXML(const TiXmlElement * element) {
+	FUNCID(AcousticSoundAbsorption::readXML);
 
 	try {
+		// search for mandatory attributes
+		if (!TiXmlAttribute::attributeByName(element, "id"))
+			throw IBK::Exception( IBK::FormatString(XML_READ_ERROR).arg(element->Row()).arg(
+				IBK::FormatString("Missing required 'id' attribute.") ), FUNC_ID);
+
+		// reading attributes
+		const TiXmlAttribute * attrib = element->FirstAttribute();
+		while (attrib) {
+			const std::string & attribName = attrib->NameStr();
+			if (attribName == "id")
+				m_id = NANDRAD::readPODAttributeValue<unsigned int>(element, attrib);
+			else if (attribName == "displayName")
+				m_displayName.setEncodedString(attrib->ValueStr());
+			else {
+				IBK::IBK_Message(IBK::FormatString(XML_READ_UNKNOWN_ATTRIBUTE).arg(attribName).arg(element->Row()), IBK::MSG_WARNING, FUNC_ID, IBK::VL_STANDARD);
+			}
+			attrib = attrib->Next();
+		}
 		// search for mandatory elements
 		// reading elements
 		const TiXmlElement * c = element->FirstChildElement();
@@ -50,17 +68,22 @@ void SoundAbsorption::readXML(const TiXmlElement * element) {
 		}
 	}
 	catch (IBK::Exception & ex) {
-		throw IBK::Exception( ex, IBK::FormatString("Error reading 'SoundAbsorption' element."), FUNC_ID);
+		throw IBK::Exception( ex, IBK::FormatString("Error reading 'AcousticSoundAbsorption' element."), FUNC_ID);
 	}
 	catch (std::exception & ex2) {
-		throw IBK::Exception( IBK::FormatString("%1\nError reading 'SoundAbsorption' element.").arg(ex2.what()), FUNC_ID);
+		throw IBK::Exception( IBK::FormatString("%1\nError reading 'AcousticSoundAbsorption' element.").arg(ex2.what()), FUNC_ID);
 	}
 }
 
-TiXmlElement * SoundAbsorption::writeXML(TiXmlElement * parent) const {
-	TiXmlElement * e = new TiXmlElement("SoundAbsorption");
+TiXmlElement * AcousticSoundAbsorption::writeXML(TiXmlElement * parent) const {
+	if (m_id == VICUS::INVALID_ID)  return nullptr;
+	TiXmlElement * e = new TiXmlElement("AcousticSoundAbsorption");
 	parent->LinkEndChild(e);
 
+	if (m_id != VICUS::INVALID_ID)
+		e->SetAttribute("id", IBK::val2string<unsigned int>(m_id));
+	if (!m_displayName.empty())
+		e->SetAttribute("displayName", m_displayName.encodedString());
 	TiXmlElement::appendSingleAttributeElement(e, "SoundAbsorption[NUM_SF]", nullptr, std::string(), IBK::val2string<double>(m_soundAbsorption[NUM_SF]));
 	return e;
 }
---
+++ externals/Vicus/src/ncg/ncg_VICUS_StructuralUnit.cpp
@@ -0,0 +1,104 @@
+/*	The NANDRAD data model library.
+
+	Copyright (c) 2012-today, Institut für Bauklimatik, TU Dresden, Germany
+
+	Primary authors:
+	  Andreas Nicolai  <andreas.nicolai -[at]- tu-dresden.de>
+	  Anne Paepcke     <anne.paepcke -[at]- tu-dresden.de>
+
+	This library is part of SIM-VICUS (https://github.com/ghorwin/SIM-VICUS)
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 3 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+*/
+
+#include <VICUS_StructuralUnit.h>
+#include <VICUS_KeywordList.h>
+
+#include <IBK_messages.h>
+#include <IBK_Exception.h>
+#include <IBK_StringUtils.h>
+#include <VICUS_Constants.h>
+#include <NANDRAD_Utilities.h>
+
+#include <tinyxml.h>
+
+namespace VICUS {
+
+void StructuralUnit::readXML(const TiXmlElement * element) {
+	FUNCID(StructuralUnit::readXML);
+
+	try {
+		// search for mandatory attributes
+		if (!TiXmlAttribute::attributeByName(element, "id"))
+			throw IBK::Exception( IBK::FormatString(XML_READ_ERROR).arg(element->Row()).arg(
+				IBK::FormatString("Missing required 'id' attribute.") ), FUNC_ID);
+
+		// reading attributes
+		const TiXmlAttribute * attrib = element->FirstAttribute();
+		while (attrib) {
+			const std::string & attribName = attrib->NameStr();
+			if (attribName == "id")
+				m_id = NANDRAD::readPODAttributeValue<unsigned int>(element, attrib);
+			else if (attribName == "displayName")
+				m_displayName = QString::fromStdString(attrib->ValueStr());
+			else {
+				IBK::IBK_Message(IBK::FormatString(XML_READ_UNKNOWN_ATTRIBUTE).arg(attribName).arg(element->Row()), IBK::MSG_WARNING, FUNC_ID, IBK::VL_STANDARD);
+			}
+			attrib = attrib->Next();
+		}
+		// search for mandatory elements
+		// reading elements
+		const TiXmlElement * c = element->FirstChildElement();
+		while (c) {
+			const std::string & cName = c->ValueStr();
+
+	//create a vector to read the data
+	std::vector<unsigned int> temp;
+			if (cName == "RoomIds"){
+				NANDRAD::readVector(c, "RoomIds", temp);
+				for (unsigned int &t: temp)
+					m_roomIds.insert(t);
+				}
+			else if (cName == "Color")
+				m_color.setNamedColor(QString::fromStdString(c->GetText()));
+			else {
+				IBK::IBK_Message(IBK::FormatString(XML_READ_UNKNOWN_ELEMENT).arg(cName).arg(c->Row()), IBK::MSG_WARNING, FUNC_ID, IBK::VL_STANDARD);
+			}
+			c = c->NextSiblingElement();
+		}
+	}
+	catch (IBK::Exception & ex) {
+		throw IBK::Exception( ex, IBK::FormatString("Error reading 'StructuralUnit' element."), FUNC_ID);
+	}
+	catch (std::exception & ex2) {
+		throw IBK::Exception( IBK::FormatString("%1\nError reading 'StructuralUnit' element.").arg(ex2.what()), FUNC_ID);
+	}
+}
+
+TiXmlElement * StructuralUnit::writeXML(TiXmlElement * parent) const {
+	if (m_id == VICUS::INVALID_ID)  return nullptr;
+	TiXmlElement * e = new TiXmlElement("StructuralUnit");
+	parent->LinkEndChild(e);
+
+	if (m_id != VICUS::INVALID_ID)
+		e->SetAttribute("id", IBK::val2string<unsigned int>(m_id));
+	if (!m_displayName.isEmpty())
+		e->SetAttribute("displayName", m_displayName.toStdString());
+
+	//convert into vector
+	std::vector<unsigned int> temp(m_roomIds.begin(), m_roomIds.end());
+	NANDRAD::writeVector(e, "RoomIds", temp);
+	if (m_color.isValid())
+		TiXmlElement::appendSingleAttributeElement(e, "Color", nullptr, std::string(), m_color.name().toStdString());
+	return e;
+}
+
+} // namespace VICUS
