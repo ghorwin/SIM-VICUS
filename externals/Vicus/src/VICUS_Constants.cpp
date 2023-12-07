e7d69888e89a03e631575696b4c0cab3ea719a12
Labauke <37376091+Labauke@users.noreply.github.com>
2023-12-07 15:02:03 +0100

    change default line weight

--- externals/Vicus/src/VICUS_Constants.cpp
+++ externals/Vicus/src/VICUS_Constants.cpp
@@ -49,9 +49,9 @@ const unsigned int SEGMENT_COUNT_ELLIPSE	= 15;
 // Multiplyer for different layers and their heights
 const double Z_MULTIPLYER					= 0.000001;
 // default line width
-const double DEFAULT_LINE_WEIGHT			= 0.1;
+const double DEFAULT_LINE_WEIGHT			= 0.05;
 // multiplier to apply to width of entities
-const double DEFAULT_LINE_WEIGHT_SCALING	= 0.1;
+const double DEFAULT_LINE_WEIGHT_SCALING	= 0.01;
 // Default scaling of fonts in DXF
 const double DEFAULT_FONT_SCALING			= 0.05;
 
